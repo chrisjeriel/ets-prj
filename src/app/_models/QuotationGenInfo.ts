c133908acfda9de56eb0e66476cef07640c875c7
yele <yelepascual17@gmail.com>
2018-11-26 14:25:36 -0800

    merge

--- src/app/_models/QuotationGenInfo.ts
+++ src/app/_models/QuotationGenInfo.ts
@@ -1,5 +1,5 @@
 export class QuotationGenInfo {
-    line: string;
+	line: string;
 	year: number;
 	seqNo: number;
 	reqSeq: string;
@@ -7,4 +7,41 @@ export class QuotationGenInfo {
 	branch: string;
 	lineClass: string;
 	policyNumber: string;
+	printedBy: string;
+	printDate: Date;
+
+	cedingCompany: string;
+	quoteStatus: string;
+	quoteDate: Date;
+	validUntil: Date;
+	requestedBy: string;
+	requestedDate: Date;
+	requestedMode: string;
+
+	principal: string;
+	contractor: string;
+	insured: string;
+	propertyProjectDescription: string;
+	site: string;
+	durationTesting: string;
+	risk: string;
+	object: string;
+	location: string;
+
+	share: string;
+	partOf100: string;
+	intermediary: string;
+
+	governmentFlag: string;
+	indicative: string;
+	openCover: string;
+	declaration: string;
+
+	openingParagraph: string;
+	closingParagraph: string;
+	createdBy: string;
+	dateCreated: Date;
+	lastUpdateBy: string;
+	lastUpdate: Date;
+
 }
--- src/app/quotation/general-info/general-info.component.ts
+++ src/app/quotation/general-info/general-info.component.ts
@@ -1,27 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { QuotationGenInfo } from '../../_models';
 
+
 @Component({
-  selector: 'app-general-info',
-  templateUrl: './general-info.component.html',
-  styleUrls: ['./general-info.component.css']
+	selector: 'app-general-info',
+	templateUrl: './general-info.component.html',
+	styleUrls: ['./general-info.component.css']
 })
 export class GeneralInfoComponent implements OnInit {
 
-  private quotationGenInfo: QuotationGenInfo;
+	private quotationGenInfo: QuotationGenInfo;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  	this.quotationGenInfo = new QuotationGenInfo();
-  	this.quotationGenInfo.line = "MCS";
-	this.quotationGenInfo.year = 2018;
-	this.quotationGenInfo.seqNo = 10230;
-	this.quotationGenInfo.reqSeq = "MOCK DATA";
-	this.quotationGenInfo.histNo = "MOCK DATA";
-	this.quotationGenInfo.branch = "MOCK DATA";
-	this.quotationGenInfo.lineClass = "MOCK DATA";
-	this.quotationGenInfo.policyNumber = "MOCK DATA";
-  }
+	ngOnInit() {
+		this.quotationGenInfo = new QuotationGenInfo();
+		this.quotationGenInfo.line = "MCS";
+		this.quotationGenInfo.year = 2222;
+		this.quotationGenInfo.seqNo = 10230;
+		this.quotationGenInfo.reqSeq = "MOCK DATA";
+		this.quotationGenInfo.histNo = "MOCK DATA";
+		this.quotationGenInfo.branch = "MOCK DATA";
+		this.quotationGenInfo.lineClass = "MOCK DATA";
+		this.quotationGenInfo.policyNumber = "MOCK DATA";
+		this.quotationGenInfo.printedBy = "";
+		this.quotationGenInfo.printDate;
+		this.quotationGenInfo.cedingCompany = "";
+		this.quotationGenInfo.quoteStatus = "";
+		this.quotationGenInfo.quoteDate;
+		this.quotationGenInfo.validUntil;
+		this.quotationGenInfo.requestedBy = "";
+		this.quotationGenInfo.requestedDate;
+		this.quotationGenInfo.requestedMode = "";
 
+	}
+
+	reqMode: SelectRequestMode[] = [
+		{ name: 'Web Portal', value: 'Web Portal' },
+		{ name: 'Fax', value: 'Fax' },
+		{ name: 'Email', value: 'Email' },
+		{ name: 'Phone', value: 'Phone' },
+		{ name: 'etc', value: 'etc' },
+	];
+
+}
+export interface SelectRequestMode {
+	name: string;
+	value: string;
 }
