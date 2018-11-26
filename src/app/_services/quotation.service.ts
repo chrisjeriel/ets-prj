9059a9b1eb8c1f8e79936e632402a0149cadc441
Christopher <chrisjeriel@gmail.com>
2018-11-26 13:31:06 +0800

    Added cust-table component.

---
+++ src/app/_services/quotation.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '@environments/environment';
+import { DummyInfo } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class QuotationService {
+
+    dummyInfoData : DummyInfo[] = [];
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    getDummyInfo() {
+        /*Dummy Data Array*/
+        this.dummyInfoData = [ 
+            new DummyInfo(1,'Christopher Jeriel','Sarsonas','Alcala', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(2,'Veronica','Raymundo','C', 'Female', 25, "January 21, 2018"), 
+            new DummyInfo(3,'Elmon','Hagacer','H', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(4,'Nadine','Lustre','R', 'Female', 25, "January 21, 2018"),
+            new DummyInfo(5,'Christopher Jeriel','Sarsonas','Alcala', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(6,'Veronica','Raymundo','C', 'Female', 25, "January 21, 2018"), 
+            new DummyInfo(7,'Elmon','Hagacer','H', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(8,'Nadine','Lustre','R', 'Female', 25, "January 21, 2018"),
+            new DummyInfo(9,'Christopher Jeriel','Sarsonas','Alcala', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(10,'Veronica','Raymundo','C', 'Female', 25, "January 21, 2018"), 
+            new DummyInfo(11,'Elmon','Hagacer','H', 'Male', 25, "January 21, 2018"), 
+            new DummyInfo(12,'Nadine','Lustre','R', 'Female', 25, "January 21, 2018"),
+        ];
+
+        /*return this.http.get<User[]>(`${environment.apiUrl}/quotation`);*/
+        return this.dummyInfoData;
+    }
+
+}
