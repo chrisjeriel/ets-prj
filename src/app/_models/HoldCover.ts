13f3e5e3f598b837098a6880eeba16387255bd4c
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2018-11-27 13:05:43 +0800

    Added model

--- src/app/_models/HoldCover.ts
+++ src/app/_models/HoldCover.ts
@@ -9,19 +9,4 @@ export class HoldCoverInfo {
 	periodTo: string;
 	requestedBy: string;
 	requestDate: string; 
-
-	/*constructor(quotationNo: number,insured: string,risk: string, holdCoverNo: number,compRefHoldCoverNo: number,
-				status: string,periodFrom: Date,periodTo: Date, requestedBy: string, requestDate: Date){
-		this.quotationNo = quotationNo;
-		this.insured = insured;
-		this.risk = risk;
-		this.holdCoverNo = holdCoverNo;
-		this.compRefHoldCoverNo = compRefHoldCoverNo;
-		this.status = status;
-		this.periodFrom = periodFrom;
-		this.periodTo = periodTo;
-		this.requestedBy = requestedBy;
-		this.requestDate = requestDate;
-
-	}*/
 }
