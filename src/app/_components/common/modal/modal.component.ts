40e8014ce6146b69653fc0cb313dfcee57b656e6
NDCaramonte <omegaxis19@gmail.com>
2018-12-05 16:57:52 +0800

    Update modal.component.ts

--- src/app/_components/common/modal/modal.component.ts
+++ src/app/_components/common/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
