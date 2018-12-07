8156c7b0f7bbe1aa84eab40166c5e917b8dfce77
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2018-12-07 13:28:08 +0800

    Revisions

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,13 +12,12 @@ import { PolicyIssuanceComponent } from './underwriting/policy-issuance/policy-i
 import { PolicyIssuanceAltComponent } from './underwriting/policy-issuance/policy-issuance-alt.component';
 import { ExtractExpiringPoliciesComponent } from  './underwriting/expiry-and-renewal/extract-expiring-policies/extract-expiring-policies.component';
 import { ExpiryListingComponent } from './underwriting/expiry-and-renewal/expiry-listing/expiry-listing.component';
-
-
 import { DummyComponent } from './_components/common/dummy/dummy.component'
 import { PolCreatePARComponent } from './underwriting/policy-issuance/pol-create-par/pol-create-par.component';
 import { PolCreateAlterationPARComponent } from './underwriting/policy-issuance/pol-create-alteration-par/pol-create-alteration-par.component';
 import { ParListingComponent } from './underwriting/policy-issuance/par-listing/par-listing.component';
 import { AltParListingComponent } from './underwriting/policy-issuance/alt-par-listing/alt-par-listing.component';
+import { HoldCoverComponent } from './quotation/hold-cover/hold-cover.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
@@ -38,6 +37,7 @@ const appRoutes: Routes = [
     { path: 'par-listing', component: ParListingComponent },
     { path: 'alt-par-listing', component: AltParListingComponent },
     { path: 'expiry-listing', component: ExpiryListingComponent },
+    { path: 'quotation-HoldCover', component: HoldCoverComponent },
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
