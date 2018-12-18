9d79d278cf963e1b2a14041eff7a9a0f3a166c25
paul26rem <paul26rem@gmail.com>
2018-12-18 13:53:44 +0800

    PolItem DOS and MLP

--- src/app/_models/PolItem.ts
+++ src/app/_models/PolItem.ts
@@ -1,4 +1,46 @@
 export class PolItem_MLP {
 	itemNo:string;
+	quantity:number;
+	description:string;
+	indemnityPeriod:number;
+	relativeImportance:number;
+	stockSpareParts:number;
+	constructor(itemNo:string,quantity:number,description:string,indemnityPeriod:number,relativeImportance:number,stockSpareParts:number){
+		this.itemNo = itemNo;
+		this.quantity = quantity;
+		this.description = description;
+		this.indemnityPeriod = indemnityPeriod;
+		this.relativeImportance = relativeImportance;
+		this.stockSpareParts = stockSpareParts;
+	}
 }
 
+export class PolGoods_DOS{
+	itemNo:string;
+	chamberNo:string;
+	typeOfGoods:string;
+	noClaimsPeriod:string;
+	sumInsured:number;
+	constructor(itemNo:string,chamberNo:string,typeOfGoods:string,noClaimsPeriod:string,sumInsured:number){
+		this.itemNo = itemNo; 
+		this.chamberNo = chamberNo; 
+		this.typeOfGoods = typeOfGoods; 
+		this.noClaimsPeriod = noClaimsPeriod; 
+		this.sumInsured = sumInsured; 
+	}
+}
+
+export class PolMachinery_DOS{
+	itemNo:string;
+	numberOfUnits:number;
+	description:string;
+	yearOfMake:number;
+	sumInsured:number;
+	constructor(itemNo:string,numberOfUnits:number,description:string,yearOfMake:number,sumInsured:number){
+		this.itemNo = itemNo; 
+		this.numberOfUnits = numberOfUnits; 
+		this.description = description; 
+		this.yearOfMake = yearOfMake; 
+		this.sumInsured = sumInsured; 
+	}
+}
--- src/app/underwriting/policy-issuance/pol-item/pol-item.component.ts
+++ src/app/underwriting/policy-issuance/pol-item/pol-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PolItem_MLP } from '@app/_models/PolItem';
+import { PolItem_MLP, PolGoods_DOS, PolMachinery_DOS } from '@app/_models/PolItem';
 import { UnderwritingService } from '@app/_services';
 
 
@@ -11,8 +11,22 @@ import { UnderwritingService } from '@app/_services';
 export class PolItemComponent implements OnInit {
 
   constructor(private underwritingService: UnderwritingService) { }
+  mlpTableData: PolItem_MLP[];
+  mlpTHeader: string[] = ['Item No','Quantity','Description of Machinery','Indemnity Period(months)','Relative Importance(%)','Spare Parts in stock standby Units'];
+  mlpDataTypes: string[] = ['text','number','text','number','percent','number'];
+
+  dosGoodsTableData: PolGoods_DOS[];
+  dosGoodsTHeader: string[] = ["Item No", "Refrigerating Chamber No", "Type of Goods", "No-Claims Period","Sum Insured"];
+  dosGoodsDataTypes:string[] = ["text","text","text","text","currency"];
+
+  dosMachineryTableData: PolMachinery_DOS[];
+  dosMachineryTHeader: string[] = ["Item No","Number of Units", "Description of Items (Technical Data including Capacity)", "Year of Make", "Sum Insured"];
+  dosMachineryDataTypes:string[] = ["text","number","text","number","currency"];
 
   ngOnInit() {
+  	this.mlpTableData = this.underwritingService.getPolItemMLPData();
+  	this.dosGoodsTableData = this.underwritingService.getPolGoodsDOSData();
+  	this.dosMachineryTableData = this.underwritingService.getPolMachineryDOSData();
   }
 
 }
