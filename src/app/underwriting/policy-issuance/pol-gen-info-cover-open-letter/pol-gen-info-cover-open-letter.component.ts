eef8a68cd03b99bb4ac406894bfc8874694de3ad
etcarino <engelcarino@gmail.com>
2018-12-19 09:41:32 +0800

    savepoint 12/19/2018 10PM

--- src/app/underwriting/policy-issuance/pol-gen-info-cover-open-letter/pol-gen-info-cover-open-letter.component.ts
+++ src/app/underwriting/policy-issuance/pol-gen-info-cover-open-letter/pol-gen-info-cover-open-letter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pol-gen-info-cover-open-letter',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PolGenInfoCoverOpenLetterComponent implements OnInit {
 
-  constructor() { }
+  constructor( private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  showItemInfoModal(content) {
+    this.modalService.open(content, { centered: true, backdrop: 'static', windowClass: "modal-size" });
   }
 
 }
