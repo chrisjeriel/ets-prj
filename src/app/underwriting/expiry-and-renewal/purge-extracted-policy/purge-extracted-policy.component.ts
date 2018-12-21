dc1bb750830f961176fbe52ec5705a538fe11a6a
paul26rem <paul26rem@gmail.com>
2018-12-21 17:05:20 +0800

    add component

---
+++ src/app/underwriting/expiry-and-renewal/purge-extracted-policy/purge-extracted-policy.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ExtractedPolicy } from '@app/_models';
+
+@Component({
+  selector: 'app-purge-extracted-policy',
+  templateUrl: './purge-extracted-policy.component.html',
+  styleUrls: ['./purge-extracted-policy.component.css']
+})
+export class PurgeExtractedPolicyComponent implements OnInit {
+  passData:any={
+  	tableData:[new ExtractedPolicy("pol no",102023,2142124,new Date(),true,false)],
+  	tHeader: ['Policy No', 'TSI Amount', 'Premium Amount', 'Expiry Date', 'P', 'X'],
+  	dataTypes:['text','currency','currency','date', 'checkbox', 'checkbox'],
+  	paginateFlag:true,
+  	infoFlag:true
+
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
