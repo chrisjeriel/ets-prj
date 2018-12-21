5c4af675ae7a59ef090f6a2909a2518ba5a206ec
yele <yelepascual17@gmail.com>
2018-12-21 17:39:48 +0800

    updated table for all the assigned screens

--- src/app/underwriting/policy-distribution/distribution-by-risk/distribution-by-risk.component.ts
+++ src/app/underwriting/policy-distribution/distribution-by-risk/distribution-by-risk.component.ts
@@ -11,16 +11,65 @@ import { Title } from '@angular/platform-browser';
 export class DistributionByRiskComponent implements OnInit {
 
   private polDistributionByRisk: DistributionByRiskInfo;
-  tableData: any[] = [];
-  tHeader: any[] = [];
-  dataTypes: any[] = [];
+  // tableData: any[] = [];
+  // tHeader: any[] = [];
+  // dataTypes: any[] = [];
   nData: DistributionByRiskInfo = new DistributionByRiskInfo(null, null, null, null, null);
+
+  tableData: any[] = [];
+  //tableDataChange: EventEmitter<any[]> = new EventEmitter<any[]>();
+  tHeader: any[] = [];
+  magnifyingGlass: any[] = ['coverCode'];
+  options: any[] = [];
+  dataTypes: any[] = [];
+  opts: any[] = [];
+  checkFlag;
+  selectFlag;
+  addFlag;
+  editFlag;
+  deleteFlag;
+  paginateFlag;
+  infoFlag;
+  searchFlag;
+
+  checkboxFlag;
+  columnId;
+  pageLength = 10;
+
+  editedData: any[] = [];
+  // editedDataChange: EventEmitter<any[]> = new EventEmitter<any[]>();
+  // rowClick: EventEmitter<any> = new EventEmitter();
+  // rowDblClick: EventEmitter<any> = new EventEmitter();
+
+
+  passData: any = {
+    tableData: [],
+    tHeader: [],
+    magnifyingGlass: [],
+    options: [],
+    dataTypes: [],
+    opts: [],
+    nData: {},
+    checkFlag: true,
+    selectFlag: false,
+    addFlag: true,
+    editFlag: false,
+    deleteFlag: true,
+    paginateFlag: true,
+    infoFlag: true,
+    searchFlag: true,
+    checkboxFlag: true,
+    pageLength: 10,
+    widths: []
+  };
+
 
   constructor(private polService: UnderwritingService, private titleService: Title) { }
 
 
   ngOnInit() {
     this.titleService.setTitle("Pol | Risk Distribution");
+
     this.tHeader.push("Treaty");
     this.tHeader.push("Ceding Company");
     this.tHeader.push("Share (%)");
@@ -34,6 +83,20 @@ export class DistributionByRiskComponent implements OnInit {
     this.dataTypes.push("text");
 
     this.tableData = this.polService.getDistByRiskData();
+
+    this.passData.tHeader.push("Treaty");
+    this.passData.tHeader.push("Ceding Company");
+    this.passData.tHeader.push("Share (%)");
+    this.passData.tHeader.push("Comm Rate (%)");
+    this.passData.tHeader.push("Line");
+
+    this.passData.dataTypes.push("text");
+    this.passData.dataTypes.push("text");
+    this.passData.dataTypes.push("percent");
+    this.passData.dataTypes.push("percent");
+    this.passData.dataTypes.push("text");
+
+    this.passData.tableData = this.polService.getDistByRiskData();
   }
 
 }
