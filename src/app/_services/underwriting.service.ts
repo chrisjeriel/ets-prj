26eaab00d31d4c602a60745c19bcfd60290426db
paul26rem <paul26rem@gmail.com>
2019-01-02 10:40:07 +0800

    added CEC editable table bugfixes

--- src/app/_services/underwriting.service.ts
+++ src/app/_services/underwriting.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
-import { DummyInfo, UnderwritingCoverageInfo, UnderwritingOtherRatesInfo, PolicyCoInsurance, PARListing, AltPARListing, ExpiryListing, CreateParTable, RenewedPolicy, PolAttachmentInfo, PolicyPrinting, PrinterList, ALOPItemInformation, UnderwritingPolicyInquiryInfo, ItemInformation, UnderwritingPolicyDistList, DistributionByRiskInfo, PolicyEndorsement, PolItem_MLP, PolGoods_DOS, PolMachinery_DOS, PolicyInwardPolBalance } from '@app/_models';
+import { DummyInfo, UnderwritingCoverageInfo, UnderwritingOtherRatesInfo, PolicyCoInsurance, PARListing, AltPARListing, ExpiryListing, CreateParTable, RenewedPolicy, PolAttachmentInfo, PolicyPrinting, PrinterList, ALOPItemInformation, UnderwritingPolicyInquiryInfo, ItemInformation, UnderwritingPolicyDistList, DistributionByRiskInfo, PolicyEndorsement, PolItem_MLP, PolGoods_DOS, PolMachinery_DOS, PolicyInwardPolBalance , PolItem_CEC} from '@app/_models';
 
 
 @Injectable({ providedIn: 'root' })
@@ -29,6 +29,7 @@ export class UnderwritingService {
     polItemMLP : PolItem_MLP[] = [];
     polGoodsDOS : PolGoods_DOS[] = [];
     polMachineryDOS: PolMachinery_DOS[] = [];
+    polCEC: PolItem_CEC[] = []; 
 
 
     constructor(private http: HttpClient) {
@@ -279,7 +280,14 @@ export class UnderwritingService {
             new PolMachinery_DOS("item 2",5, "desc", 2, 506),
         ];
         return this.polMachineryDOS;
+    }
 
+    getPolCECData(){
+        this.polCEC = [
+            new PolItem_CEC('item 1','Item and Location','dedctibles',10000),
+            new PolItem_CEC('item 2','Item and Location','dedctibles',10000),
+        ]
+        return this.polCEC;
     }
 
 
