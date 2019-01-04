a6692a6ee3cf8879628c42b00ee0fcb95e087997
etcarino <engelcarino@gmail.com>
2019-01-04 11:06:02 +0800

    savepoint 11:00 AM

--- src/app/quotation/attachment/attachment.component.ts
+++ src/app/quotation/attachment/attachment.component.ts
@@ -14,15 +14,48 @@ import { Title } from '@angular/platform-browser';
 
 export class AttachmentComponent implements OnInit {
 
-  dtOptions: DataTables.Settings = {};
+ /* dtOptions: DataTables.Settings = {};*/
   tableData: any[] = [];
   tHeader: any[] = [];
   options: any[] = [];
   dataTypes: any[] = [];
-  pageLength: number;
+  opts: any[] = [];
+  checkFlag;
+  selectFlag;
+  addFlag;
+  editFlag;
+  deleteFlag;
+  paginateFlag;
+  infoFlag;
+  searchFlag;
+  checkboxFlag;
+  columnId;
+  pageLength;
+  editedData: any[] = [];
   nData: AttachmentInfo = new AttachmentInfo(null, null);
   test: boolean = true;
   private attachmentInfo: AttachmentInfo;
+
+  passData: any = {
+    tableData: [],
+    tHeader: [],
+    magnifyingGlass: [],
+    options: [],
+    dataTypes: [],
+    opts: [],
+    nData: new AttachmentInfo(null, null),
+    checkFlag: true,
+    selectFlag: false,
+    addFlag: true,
+    editFlag: false,
+    deleteFlag: true,
+    paginateFlag: false,
+    infoFlag: false,
+    searchFlag: true,
+    checkboxFlag: true,
+    pageLength: 10,
+    widths: []
+  };
 
   constructor(config: NgbDropdownConfig,
     private quotationService: QuotationService, private titleService: Title) {
@@ -32,23 +65,27 @@ export class AttachmentComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.titleService.setTitle("Quo | Attachment");
+
+   /* this.titleService.setTitle("Quo | Attachment");
     this.tHeader.push("File Name");
     this.tHeader.push("Description");
     this.tHeader.push("Actions");
-
     // this.options.push("");
     // this.options.push("Q - Quotation");
     // this.options.push("P - Policy");
     // this.options.push("C - Claim");
     // this.options.push("A - Accounting");
-
     // this.dataTypes.push("text");
     // this.dataTypes.push("text");
     // this.dataTypes.push("select1");
     // this.dataTypes.push("text");
+    this.tableData = this.quotationService.getAttachment();*/
 
-    this.tableData = this.quotationService.getAttachment();
+    this.passData.tHeader.push("File Name");
+    this.passData.tHeader.push("Description");
+    this.passData.tHeader.push("Actions");
+    this.passData.tableData = this.quotationService.getAttachment();
+
   }
 
 }
