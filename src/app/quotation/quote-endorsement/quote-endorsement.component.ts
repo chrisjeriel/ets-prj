bb5efee553e762fcb48079460afd789deda5672f
NDCaramonte <omegaxis19@gmail.com>
2019-01-07 12:42:28 +0800

    Quotation Endorsement revisions

--- src/app/quotation/quote-endorsement/quote-endorsement.component.ts
+++ src/app/quotation/quote-endorsement/quote-endorsement.component.ts
@@ -5,52 +5,79 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-quote-endorsement',
-  templateUrl: './quote-endorsement.component.html',
-  styleUrls: ['./quote-endorsement.component.css']
+    selector: 'app-quote-endorsement',
+    templateUrl: './quote-endorsement.component.html',
+    styleUrls: ['./quote-endorsement.component.css']
 })
 export class QuoteEndorsementComponent implements OnInit {
-  private quotationInfo: QuotationInfo;
-  private quoteEndorsement: QuoteEndorsement;
-  dtOptions: DataTables.Settings = {};
+    private quotationInfo: QuotationInfo;
+    private quoteEndorsement: QuoteEndorsement;
+    dtOptions: DataTables.Settings = {};
 
-  quoteOptionTableData: any[] = [];
-  quoteOptionTHeader: any[] = ['Option No', 'Rate(%)', 'Conditions', 'Comm Rate Quota(%)', 'Comm Rate Surplus(%)', 'Comm Rate Fac(%)'];
-  quoteOptionDataType: any[] = ['text', 'percent', 'text', 'percent', 'percent', 'percent', 'percent'];
-  quoteOptionNData: QuotationOption = new QuotationOption(null, null, null, null, null, null);
-  quoteOptionMagnifyingGlass: any[] = ['conditions'];
-  quoteOptionEdited: QuotationOption[] = [];
+    quoteOptionTableData: any[] = [];
+    quoteOptionTHeader: any[] = ['Option No', 'Rate(%)', 'Conditions', 'Comm Rate Quota(%)', 'Comm Rate Surplus(%)', 'Comm Rate Fac(%)'];
+    quoteOptionDataType: any[] = ['text', 'percent', 'text', 'percent', 'percent', 'percent', 'percent'];
+    quoteOptionNData: QuotationOption = new QuotationOption(null, null, null, null, null, null);
+    quoteOptionMagnifyingGlass: any[] = ['conditions'];
+    quoteOptionEdited: QuotationOption[] = [];
 
-  tableData: any[] = [];
-  tHeader: any[] = ['Endt Code', 'Endt Title', 'Endt Description', 'Remarks'];
-  magnifyingGlass: any[] = ["endtCode"]
-  nData: QuoteEndorsement = new QuoteEndorsement(null, null, null, null, null);
+    tableData: any[] = [];
+    tHeader: any[] = ['Endt Code', 'Endt Title', 'Endt Description', 'Remarks'];
+    magnifyingGlass: any[] = ["endtCode"]
+    nData: QuoteEndorsement = new QuoteEndorsement(null, null, null, null, null);
 
-  optionNos: number[] = [];
-  constructor(private quotationService: QuotationService, private modalService: NgbModal, private titleService: Title) { }
+    optionNos: number[] = [];
+
+    optionsData: any = {
+        tableData: this.quotationService.getQuoteOptions(),
+        tHeader: ['Option No', 'Rate(%)', 'Conditions', 'Comm Rate Quota(%)', 'Comm Rate Surplus(%)', 'Comm Rate Fac(%)'],
+        dataTypes: ['text', 'percent', 'text', 'percent', 'percent', 'percent', 'percent'],
+        resizable: [false, false, true, false, false, false],
+        pagination: true,
+        pageStatus: true,
+        tableOnly: true,
+        pageLength: 3,
+    }
+    
+    endorsementData: any = {
+        tableData: this.quotationService.getEndorsements(1),
+        tHeader: ['Endt Code', 'Endt Title', 'Endt Description', 'Remarks'],
+        magnifyingGlass: ['endtCode'],
+        nData: new QuoteEndorsement(null, null, null, null, null),
+        checkFlag: true,
+        addFlag: true,
+        deleteFlag: true,
+        infoFlag: true,
+        paginateFlag: true,
+    }
+    
+    constructor(private quotationService: QuotationService, private modalService: NgbModal, private titleService: Title) { }
 
 
-  ngOnInit() {
-    this.titleService.setTitle("Quo | Endorsement");
-    this.dtOptions = {
-      ordering: false,
-      pagingType: 'simple_numbers',
-      lengthChange: false,
-      searching: false,
-      info: false,
-    };
-    this.optionNos = this.quotationService.getQuoteOptionNos();
-    this.quotationInfo = new QuotationInfo();
-    this.quotationInfo.quotationNo = "SMP-0000-0000-00";
-    this.quotationInfo.insuredName = "Insured Name";
-    this.quoteOptionTableData = this.quotationService.getQuoteOptions();
+    ngOnInit() {
+        this.titleService.setTitle("Quo | Endorsement");
+        this.dtOptions = {
+            ordering: false,
+            pagingType: 'simple_numbers',
+            lengthChange: false,
+            searching: false,
+            info: false,
+        };
+        this.optionNos = this.quotationService.getQuoteOptionNos();
+        this.quotationInfo = new QuotationInfo();
+        this.quotationInfo.quotationNo = "SMP-0000-0000-00";
+        this.quotationInfo.insuredName = "Insured Name";
+        this.quoteOptionTableData = this.quotationService.getQuoteOptions();
 
-    this.tableData = this.quotationService.getEndorsements(1);
+        this.tableData = this.quotationService.getEndorsements(1);
 
-  }
+    }
 
-  clickRow(event) {
-    console.log(event);
-    this.tableData = this.quotationService.getEndorsements(event.target.closest("tr").children[1].innerText);
-  }
+    clickRow(event) {
+        console.log(event);
+        this.tableData = this.quotationService.getEndorsements(event.target.closest("tr").children[1].innerText);
+    }
+    save(){
+        //do something
+    }
 }
