913b57e6b56fb09003f779346472aeaa692fe0cc
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2019-01-09 14:00:47 +0800

    Revisions

--- src/app/_directives/required.directive.ts
+++ src/app/_directives/required.directive.ts
@@ -9,20 +9,20 @@ export class RequiredDirective {
     constructor(private er: ElementRef) { 
 
     }
-    @HostListener('blur', ['$event.target.value']) onBlur(value){
-        if(value === null || typeof value === 'undefined' || value == ''){
-            highlight(this.er);
-        }else{
-            unHighlight(this.er);
-        }
-    }
+    // @HostListener('blur', ['$event.target.value']) onBlur(value){
+    //     if(value === null || typeof value === 'undefined' || value == ''){
+    //         highlight(this.er);
+    //     }else{
+    //         unHighlight(this.er);
+    //     }
+    // }
     
-    @HostListener('ngModelChange', ['$event']) onNgModelChange(value){
-        if(value === null || typeof value === 'undefined' || value == ''){
-            highlight(this.er);
-        }else{
-            unHighlight(this.er);
-        }
-    }
+    // @HostListener('ngModelChange', ['$event']) onNgModelChange(value){
+    //     if(value === null || typeof value === 'undefined' || value == ''){
+    //         highlight(this.er);
+    //     }else{
+    //         unHighlight(this.er);
+    //     }
+    // }
 
 }
