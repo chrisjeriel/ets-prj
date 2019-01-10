09eff0fe5d84597018bfeb6d2f5aec7929487a68
Mike Bryan <m.bryanmartinez@gmail.com>
2019-01-10 12:00:36 +0800

    claim new screens

--- src/app/claims/claim/clm-claim-processing/clm-section-covers/clm-section-covers.component.ts
+++ src/app/claims/claim/clm-claim-processing/clm-section-covers/clm-section-covers.component.ts
@@ -7,41 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClmSectionCoversComponent implements OnInit {
 
+  /*passData: any = {
+        tableData: [],
+        tHeader: [],
+        dataTypes: [],
+        resizable: [],
+        filters: [],
+        pageLength: 10,
+        expireFilter: false,
+        checkFlag: false,
+        tableOnly: false,
+        fixedCol: false,
+        printBtn: false,
+        pageStatus: false,
+        pagination: false,
+        addFlag: false,
+        editFlag: false,
+        deleteFlag: false,
+        copyFlag: false,
+        pageID: 1
+    }*/
+
   passData: any = {
-		tableData:[],
-		tHeader:[],
-		tHeaderWithColspan:[],
-		magnifyingGlass:[],
-		options:[],
-		dataTypes:[],
-		opts:[],
-		nData:{},
-		checkFlag:false,
-		selectFlag:false,
-		addFlag:false,
-		editFlag:false,
-		deleteFlag:false,
-		paginateFlag: false,
-		infoFlag:false,
-		searchFlag:false,
-		checkboxFlag:false,
-		pageLength:10,
-		widths: [],
-		pageID:1,
-	};
+    tableData: [],
+    tHeader: [],
+    dataTypes: [],
+    paginateFlag: true,
+    infoFlag: true,
+    searchFlag: true,
+    pageLength: 5,
+    widths: [],
+    pageID: 1
+  };
 
   constructor() { }
 
   ngOnInit() {
   	this.passData.tHeader.push("Deductible Code", "Deductible Title", "Rate (%)", "Amount", "Deductible Text");
   	this.passData.tableData.push(["AOG30", "ACTS OF GOD 30", "0.50000000000","","Acts of Nature - Php 1,800,000.00 each and every loss"],
-  								 ["AOC31", "39,000 - AOC", "","39,000.00","Any Other Cause - Php 39,000.00 for each and every loss"]);
+  								 ["AOC31", "39,000 - AOC", "","39000","Any Other Cause - Php 39,000.00 for each and every loss"]);
   	this.passData.dataTypes.push("text", "text", "percent", "currency", "text");
   	this.passData.widths.push("1","auto","auto","auto","auto");
-  	this.passData.searchFlag = true;
-  	this.passData.paginateFlag = true;
-  	this.passData.infoFlag = true;
-  	this.passData.pageLength = 5;
   }
 
 }
