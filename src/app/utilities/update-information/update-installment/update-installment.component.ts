195cc5ad3f6367c2c01a4891490c024ea58b7674
yele <yelepascual17@gmail.com>
2019-01-14 16:38:46 +0800

    updated pol-util-update-installment

--- src/app/utilities/update-information/update-installment/update-installment.component.ts
+++ src/app/utilities/update-information/update-installment/update-installment.component.ts
@@ -7,7 +7,6 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./update-installment.component.css']
 })
 export class UpdateInstallmentComponent implements OnInit {
-  magnifyingGlass;
 
   passDataInstallmentInfo: any = {
     tableData: [["1", "2019-01-12", "01/01/2019", "300000", "3.2", "500000", "100000", "200000"]],
@@ -16,6 +15,21 @@ export class UpdateInstallmentComponent implements OnInit {
     addFlag: true,
     deleteFlag: true,
     pageID: 1,
+    widths: ["1", "auto", "auto", "auto", "auto", "auto", "auto", "auto"],
+    nData: [null, null, null, null, null, null, null, null],
+    pageLength: 5
+  };
+
+  passDataOtherCharges: any = {
+    tableData: [["101", "Description 101", "50000"]],
+    tHeader: ["Code", "Charge Description", "Amount"],
+    dataTypes: ["text", "text", "currency"],
+    addFlag: true,
+    deleteFlag: true,
+    pageID: 2,
+    nData: [null, null, null],
+    magnifyingGlass: ['0'],
+    pageLength: 5
   };
 
   constructor(private titleService: Title) { }
