c451eb3454913f7f5e45f57c34b05fbde04e1f73
paul26rem <paul26rem@gmail.com>
2019-01-15 09:33:48 +0800

    Added total for editable table

--- src/app/_components/common/dummy/dummy.component.ts
+++ src/app/_components/common/dummy/dummy.component.ts
@@ -111,9 +111,10 @@ export class DummyComponent implements OnInit {
 
     passDataEditable: any = {
         tableData: [],
-        tHeader: ["ID", "First Name", "Last Name", "Middle Name", "Gender", "Age", "Birth Date"],
+        tHeader: ["ID", "First Name", "Last Name", "Middle Name", "Gender", "Age", "Birth Date","Actions"],
+        total : [null,null,null,null,"Total","age",null,null],
         nData: new DummyInfo(null, null, null, null, null, null, null),
-        dataTypes: ['number','text','text','text','text','number','text'],
+        dataTypes: ['number','text','text','text','text','number','date'],
         checkFlag:true,
         addFlag:true,
         deleteFlag:true,
@@ -122,7 +123,8 @@ export class DummyComponent implements OnInit {
         searchFlag:true,
         infoFlag: true,
         paginateFlag: true,
-        magnifyingGlass:['id']
+        magnifyingGlass:['id'],
+        uneditable:[true,false,false,false,true,true,true],
     };
         
     constructor(private quotationService: QuotationService, private modalService: NgbModal) { 
