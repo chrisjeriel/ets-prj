3d349e3f993e1c9dd92d3dac1a8bcf70f87d0538
paul26rem <paul26rem@gmail.com>
2019-01-17 12:20:34 +0800

    revision for claims 1.1, 1.2, 1.3, 2.0

--- src/app/_models/Claims-History.ts
+++ src/app/_models/Claims-History.ts
@@ -3,6 +3,7 @@ export class ClaimsHistoryInfo {
 	histNo: string;
 	histType: string;
 	type: string;
+	exGratia:boolean;
 	currency: string;
 	reserve: number;
 	paymentAmount: number;
@@ -10,10 +11,11 @@ export class ClaimsHistoryInfo {
 	refDate: Date;
 	remarks: string;
 
-	constructor(histNo: string, histType: string, type: string, currency: string, reserve: number, paymentAmount: number, refNo: string, refDate: Date, remarks: string) {
+	constructor(histNo: string, histType: string, type: string,exGratia:boolean, currency: string, reserve: number, paymentAmount: number, refNo: string, refDate: Date, remarks: string) {
 		this.histNo = histNo;
 		this.histType = histType;
 		this.type = type;
+		this.exGratia = exGratia;
 		this.currency = currency;
 		this.reserve = reserve;
 		this.paymentAmount = paymentAmount;
