3b686f06044b8804bedfa1bac13679af248c502d
etcarino <engelcarino@gmail.com>
2019-01-18 08:49:18 +0800

    savepoint 1/18/2019

--- src/app/_components/common/modal/modal.component.ts
+++ src/app/_components/common/modal/modal.component.ts
@@ -6,7 +6,7 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
     templateUrl: './modal.component.html',
     styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit, AfterViewInit {
+export class ModalComponent implements OnInit/*, AfterViewInit*/ {
 
     @Input() mdlConfig = {
         mdlType: "",
@@ -36,7 +36,7 @@ export class ModalComponent implements OnInit, AfterViewInit {
     ngOnInit() {
 
     }
-   /* ngAfterViewInit(){
+/*    ngAfterViewInit(){
         console.log(this.test);
         console.log("content >>> " + this.content);
     }*/
