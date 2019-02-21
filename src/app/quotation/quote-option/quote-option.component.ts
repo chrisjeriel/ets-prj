313af790db1e119e624cd9d862323c09daf01706
etcarino <engelcarino@gmail.com>
2019-02-21 10:48:17 +0800

    savepoint 2/21/2019

--- src/app/quotation/quote-option/quote-option.component.ts
+++ src/app/quotation/quote-option/quote-option.component.ts
@@ -3,7 +3,7 @@ import { QuotationInfo, QuotationOption, QuotationOtherRates, QuotationDeductibl
 import { QuotationService } from '../../_services';
 import { Title } from '@angular/platform-browser';
 import { CustEditableNonDatatableComponent } from '@app/_components/common/cust-editable-non-datatable/cust-editable-non-datatable.component';
-
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -16,6 +16,8 @@ export class QuoteOptionComponent implements OnInit {
     private quotationInfo: QuotationInfo;
     private quotationOption: QuotationOption;
     private quotationOtherRates: QuotationOtherRates;
+    private sub: any;
+
 
     quoteOptionTableData: any[] = [];
     quoteOptionTHeader: any[] = ['Option No', 'Rate(%)', 'Conditions', 'Comm Rate Quota(%)', 'Comm Rate Surplus(%)', 'Comm Rate Fac(%)'];
@@ -30,6 +32,13 @@ export class QuoteOptionComponent implements OnInit {
     otherRatesDataType: any[] = ['text', 'currency', 'text'];
     otherRatesMagnify: any[] = ['others', 'deductible'];
     otherRatesNData: QuotationOtherRates = new QuotationOtherRates(null, null, null, null);
+    
+    quoteNoData: string;
+    quotationNum: string;
+    insured: any;
+    riskName: any;
+    quoteId: any;
+    from: string;
 
     optionsData: any = {
         tableData: [],
@@ -82,7 +91,7 @@ export class QuoteOptionComponent implements OnInit {
     }
 
     record: any[];
-    constructor(private quotationService: QuotationService, private titleService: Title) { }
+    constructor(private quotationService: QuotationService, private titleService: Title, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.titleService.setTitle("Quo | Quote Option");
@@ -90,47 +99,77 @@ export class QuoteOptionComponent implements OnInit {
         this.quotationInfo.quotationNo = "SMP-0000-0000-00";
         this.quotationInfo.insuredName = "Insured Name";
 
+        if (this.quotationService.toGenInfo[0] == "edit") {
 
-        this.quotationService.getQuoteOptions().subscribe(data => {  
+            this.sub = this.route.params.subscribe(params => {
+                this.from = params['from'];
+                    if (this.from == "quo-processing") {
+                        this.quotationNum = params['quotationNo'];
+                    }
+            });
+
+             this.quotationService.getQuoteGenInfo(null,this.plainQuotationNo(this.quotationNum)).subscribe((data: any) => {
+                        this.insured = data.quotationGeneralInfo.insuredDesc; 
+                        this.quoteNoData = data.quotationGeneralInfo.quotationNo;
+                        if(data.project == null){
+                             this.riskName = null;
+                        } else {
+                            this.riskName = data.project.riskName; 
+                        }
+                        this.quoteId = data.quotationGeneralInfo.quoteId.toString();
+                        console.log(this.quoteId);
+            });
+
+
+
+
+            this.quotationService.getQuoteOptions().subscribe(data => {  
             var optionRecords = data['quotation'].optionsList;
 
-            for(let rec of optionRecords){
-                this.optionsData.tableData.push(new QuotationOption(
-                    rec.optionId, 
-                    rec.optionRt, 
-                    rec.condition, 
-                    rec.commRtQuota, 
-                    rec.commRtSurplus, 
-                    rec.commRtFac
-                ));                
-            }
+                    for(let rec of optionRecords){
+                        this.optionsData.tableData.push(new QuotationOption(
+                            rec.optionId, 
+                            rec.optionRt, 
+                            rec.condition, 
+                            rec.commRtQuota, 
+                            rec.commRtSurplus, 
+                            rec.commRtFac
+                        ));                
+                    }
 
 
-            for(let rec of optionRecords){
-                for(let r of rec.deductiblesList){                  
-                    this.deductiblesData.tableData.push(new QuotationDeductibles(
-                        r.deductibleCd,
-                        r.deductibleTitle,
-                        r.deductibleRt,
-                        r.deductibleAmt,
-                        r.deductibleTxt
-                    ));
-                }               
-            }
+                    for(let rec of optionRecords){
+                        for(let r of rec.deductiblesList){                  
+                            this.deductiblesData.tableData.push(new QuotationDeductibles(
+                                r.deductibleCd,
+                                r.deductibleTitle,
+                                r.deductibleRt,
+                                r.deductibleAmt,
+                                r.deductibleTxt
+                            ));
+                        }               
+                    }
 
             var otherRatesRecords = data['quotation'].otherRatesList;
 
-            for(let rec of otherRatesRecords){
-                this.otherRatesData.tableData.push(new QuotationOtherRates(
-                    rec.coverCd, 
-                    rec.coverCdDesc, 
-                    rec.rate, 
-                    rec.amount                    
-                ));                
-            }
-            
-            this.table.forEach(table => { table.refreshTable() });
-        });
+                for(let rec of otherRatesRecords){
+                    this.otherRatesData.tableData.push(new QuotationOtherRates(
+                        rec.coverCd, 
+                        rec.coverCdDesc, 
+                        rec.rate, 
+                        rec.amount                    
+                    ));                
+                }
+                
+                this.table.forEach(table => { table.refreshTable() });
+            });
+
+        } else {
+
+
+        }
+        
+
     }
 
     save() {
@@ -141,6 +180,12 @@ export class QuoteOptionComponent implements OnInit {
         //console.log(event);
         //this.otherRatesTableData = this.quotationService.getQuotataionOtherRates(event.target.closest("tr").children[1].children[0].children[1].value);
     }
+
+    plainQuotationNo(data: string){
+        var arr = data.split('-');
+        return arr[0] + '-' + arr[1] + '-' + parseInt(arr[2]) + '-' + parseInt(arr[3]) + '-' + parseInt(arr[4]);
+    }
+
 
 
 }
