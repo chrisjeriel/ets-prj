3a23709d5d061bee7af37c921550d0ce8afd996c
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2019-02-26 18:12:47 +0800

    Service Revisions

--- src/app/accounting-service/utilities/batch-or-printing/batch-or-printing.component.ts
+++ src/app/accounting-service/utilities/batch-or-printing/batch-or-printing.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { BatchOR } from '@app/_models';
 import { AccountingService } from '@app/_services';
+import { ActivatedRoute,Router } from '@angular/router';
+import { NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-batch-or-printing',
@@ -9,7 +11,10 @@ import { AccountingService } from '@app/_services';
   styleUrls: ['./batch-or-printing.component.css']
 })
 export class BatchOrPrintingComponent implements OnInit {
-  
+  exitLink: string;
+  exitTab: string;
+  sub: any;
+
   PassData: any = {
   	tableData: this.accountingService.getBatchOR(),
   	tHeader: ['G', 'P', 'OR Date', 'OR', 'Number', 'Payor','Amount'],
@@ -22,9 +27,14 @@ export class BatchOrPrintingComponent implements OnInit {
   	widths: [50,50,  100,100, 250, 'auto', 150]
   }
 
-  constructor(private accountingService: AccountingService) { }
+  constructor(private accountingService: AccountingService,private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
 
+  onTabChange($event: NgbTabChangeEvent) {
+    if ($event.nextId === 'Exit') {
+        this.router.navigateByUrl('');
+      } 
+  }
 }
