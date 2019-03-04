5a8e61bd0bded0cff5b8046b226f3b5f467afdfb
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2019-03-04 10:45:56 +0800

    Before currencyRate directive

--- src/app/quotation/coverage/coverage.component.ts
+++ src/app/quotation/coverage/coverage.component.ts
@@ -117,7 +117,6 @@ export class CoverageComponent implements OnInit {
       this.quoteNo += '-' + parseInt(this.quotationInfo.quotationNo.split(/[-]/g)[i]);
     } 
 
-
     this.riskId = this.quotationInfo.riskId;
 
 
@@ -158,9 +157,6 @@ export class CoverageComponent implements OnInit {
 
       this.coverageData.currencyCd = this.quotationInfo.currencyCd;
       this.coverageData.currencyRt = this.quotationInfo.currencyRt;
-      this.lineCd      = this.quoteNo.split('-')[0];
-
-    console.log(this.coverageData.currencyCd)
     
 
     // this.quotationCoverageInfo = new QuotationCoverageInfo(null, null, null, null, null, null, null, null);
@@ -174,34 +170,40 @@ export class CoverageComponent implements OnInit {
     // this.quotationCoverageInfo.remarks = "MOCK DATA";
   }
 
+
+  prepareSaveData(){
+    this.lineCd      = this.quoteNo.split('-')[0];
+    this.editedData  = [];
+    this.deletedData = [];
+
+    for (var i = 0 ; this.passData.tableData.length > i; i++) {
+       if(this.passData.tableData[i].edited && !this.passData.tableData[i].deleted ){
+           this.editedData.push(this.passData.tableData[i]);
+           this.editedData[this.editedData.length-1].createDate = new Date(this.editedData[this.editedData.length-1].createDate[0],this.editedData[this.editedData.length-1].createDate[1]-1,this.editedData[this.editedData.length-1].createDate[2]).toISOString();
+           this.editedData[this.editedData.length-1].updateDate = new Date(this.editedData[this.editedData.length-1].updateDate[0],this.editedData[this.editedData.length-1].updateDate[1]-1,this.editedData[this.editedData.length-1].updateDate[2]).toISOString();
+           this.editedData[this.editedData.length-1].lineCd     = this.lineCd;
+       }else if(this.passData.tableData[i].edited && this.passData.tableData[i].deleted){
+         this.deletedData.push(this.passData.tableData[i]);
+         this.deletedData[this.deletedData.length-1].createDate = new Date(this.deletedData[this.deletedData.length-1].createDate[0],this.deletedData[this.deletedData.length-1].createDate[1]-1,this.deletedData[this.deletedData.length-1].createDate[2]).toISOString();
+         this.deletedData[this.deletedData.length-1].updateDate = new Date(this.deletedData[this.deletedData.length-1].updateDate[0],this.deletedData[this.deletedData.length-1].updateDate[1]-1,this.deletedData[this.deletedData.length-1].updateDate[2]).toISOString();
+         this.deletedData[this.deletedData.length-1].lineCd = this.lineCd;
+       }
+     }
+     this.coverageData.createDate          = new Date(this.coverageData.createDate[0],this.coverageData.createDate[1]-1,this.coverageData.createDate[2]).toISOString();
+     //this.coverageData.updateDate          = new Date(this.coverageData.updateDate[0],this.coverageData.updateDate[1]-1,this.coverageData.updateDate[2]).toISOString();
+     this.coverageData.saveSectionCovers   = this.editedData;
+     this.coverageData.deleteSectionCovers = this.deletedData;
+     this.coverageData.quoteId             = this.quotationInfo.quoteId;
+     this.coverageData.projId              = 1;
+     this.coverageData.riskId              = this.riskId;
+
+
+  }
+
+
   saveData(){
-   this.lineCd      = this.quoteNo.split('-')[0];
-   this.editedData  = [];
-   this.deletedData = [];
-
-   for (var i = 0 ; this.passData.tableData.length > i; i++) {
-      if(this.passData.tableData[i].edited && !this.passData.tableData[i].deleted ){
-          this.editedData.push(this.passData.tableData[i]);
-          this.editedData[this.editedData.length-1].createDate = new Date(this.editedData[this.editedData.length-1].createDate[0],this.editedData[this.editedData.length-1].createDate[1]-1,this.editedData[this.editedData.length-1].createDate[2]).toISOString();
-          this.editedData[this.editedData.length-1].updateDate = new Date(this.editedData[this.editedData.length-1].updateDate[0],this.editedData[this.editedData.length-1].updateDate[1]-1,this.editedData[this.editedData.length-1].updateDate[2]).toISOString();
-          this.editedData[this.editedData.length-1].lineCd     = this.lineCd;
-      }else if(this.passData.tableData[i].edited && this.passData.tableData[i].deleted){
-        this.deletedData.push(this.passData.tableData[i]);
-        this.deletedData[this.deletedData.length-1].createDate = new Date(this.deletedData[this.deletedData.length-1].createDate[0],this.deletedData[this.deletedData.length-1].createDate[1]-1,this.deletedData[this.deletedData.length-1].createDate[2]).toISOString();
-        this.deletedData[this.deletedData.length-1].updateDate = new Date(this.deletedData[this.deletedData.length-1].updateDate[0],this.deletedData[this.deletedData.length-1].updateDate[1]-1,this.deletedData[this.deletedData.length-1].updateDate[2]).toISOString();
-        this.deletedData[this.deletedData.length-1].lineCd = this.lineCd;
-      }
-    }
-    this.coverageData.createDate          = new Date(this.coverageData.createDate[0],this.coverageData.createDate[1]-1,this.coverageData.createDate[2]).toISOString();
-    //this.coverageData.updateDate          = new Date(this.coverageData.updateDate[0],this.coverageData.updateDate[1]-1,this.coverageData.updateDate[2]).toISOString();
-    this.coverageData.saveSectionCovers   = this.editedData;
-    this.coverageData.deleteSectionCovers = this.deletedData;
-    this.coverageData.quoteId             = this.quotationInfo.quoteId;
-    this.coverageData.projId              = 1;
-    this.coverageData.riskId              = this.riskId;
-
-
-
+    
+    this.prepareSaveData();
 
     if(this.editedData.length < 1 && this.deletedData.length < 1){
         this.errorMdlMessage = "No changes were made!"
@@ -256,6 +258,7 @@ export class CoverageComponent implements OnInit {
   }
 
   update(event){
+      this.lineCd = this.quoteNo.split('-')[0];
         this.coverageData.sectionISi =0;
         this.coverageData.sectionIISi =0;
         this.coverageData.sectionIIISi =0;
@@ -275,7 +278,15 @@ export class CoverageComponent implements OnInit {
 
          }
       }
+
+     if(this.lineCd == 'CAR' || this.lineCd == 'EAR'){
+        this.coverageData.totalSi = this.coverageData.sectionISi + this.coverageData.sectionIISi;
+     } else if (this.lineCd == 'EEI'){
        this.coverageData.totalSi = this.coverageData.sectionISi + this.coverageData.sectionIISi + this.coverageData.sectionIIISi;
+     } else{
+       this.coverageData.totalSi = this.coverageData.sectionISi
+     }
+     
      this.focusBlur();
   }
 
