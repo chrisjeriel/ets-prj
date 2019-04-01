ec6c17797ee4f7daba68ead99a7777603faed57a
Christopher <chrisjeriel@gmail.com>
2019-04-01 17:21:59 +0800

    Changes in export to CSV.

--- src/app/quotation/quotation-processing/quotation-processing.component.ts
+++ src/app/quotation/quotation-processing/quotation-processing.component.ts
@@ -158,6 +158,7 @@ export class QuotationProcessingComponent implements OnInit {
         pageLength: 20,
         expireFilter: false, checkFlag: false, tableOnly: false, fixedCol: false, printBtn: false, addFlag: true, editFlag: true, copyFlag: false, pageStatus: true, pagination: true, pageID: 1,
         keys: ['quotationNo','cessionDesc','lineClassCdDesc','status','cedingName','principalName','contractorName','insuredDesc','riskName','objectDesc','site','policyNo','currencyCd','issueDate','expiryDate','reqBy','createUser'],
+        colSize: ['', '', '60px', '', '', '', '', '', '', '', '', '', '', '', '', ''],
         genericBtn1: 'Copy Quote Details',
         genericBtn2: 'Internal Competition',
         exportFlag: true,
