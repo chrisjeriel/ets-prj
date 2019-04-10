b57bc87b259cec607f55dd7883e7d55cf724ccf3
zaragosa.melanie <cpi>
2019-04-10 19:53:57 +0800

    retrieve policy record for update installment

--- src/app/utilities/update-information/update-installment/update-installment.component.ts
+++ src/app/utilities/update-information/update-installment/update-installment.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { UnderwritingService, NotesService } from '../../../_services';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CustNonDatatableComponent } from '@app/_components/common/cust-non-datatable/cust-non-datatable.component';
 
 @Component({
   selector: 'app-update-installment',
@@ -7,6 +10,21 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./update-installment.component.css']
 })
 export class UpdateInstallmentComponent implements OnInit {
+  searchParams: any[] = [];
+  @ViewChildren(CustNonDatatableComponent) table: QueryList<CustNonDatatableComponent>;
+  selectedPolicy: any = null;
+  polNo: any[] = [];
+  policyId: any = "";
+  condition: any = "";
+  cedingName: any = "";
+  insuredDesc: any = "";
+  riskName: any = "";
+  inceptionDate: any = "";
+  inceptionTime: any = "";
+  expiryDate: any = "";
+  expiryTime: any = "";
+  selected: any;
+  fetchedData: any;
 
   passDataInstallmentInfo: any = {
     tableData: [["1", "2019-01-12", "01/01/2019", "300000", "3.2", "500000", "100000", "200000"]],
@@ -32,10 +50,91 @@ export class UpdateInstallmentComponent implements OnInit {
     pageLength: 5
   };
 
-  constructor(private titleService: Title) { }
+  passDataLOV: any = {
+    tableData: [],
+    tHeader:["Policy No", "Ceding Company", "Insured", "Risk"],  
+    dataTypes: ["text","text","text","text"],
+    pageLength: 10,
+    resizable: [false,false,false,false],
+    tableOnly: false,
+    keys: ['policyNo','cedComp','insured','risk'],
+    // keys: ['openPolicyNo','cedingName','insuredDesc','riskName'],
+    pageStatus: true,
+    pagination: true,
+    filters: [
+    /*{key: 'quotationNo', title: 'Quotation No.',dataType: 'seq'},
+    {key: 'cedingName',title: 'Ceding Co.',dataType: 'text'},
+    {key: 'insuredDesc',title: 'Insured',dataType: 'text'},
+    {key: 'riskName',title: 'Risk',dataType: 'text'}*/],
+    pageID: 'createPolLov'
+  }
+
+  constructor(private underwritingService: UnderwritingService, private modalService: NgbModal,private ns: NotesService, private titleService: Title) { }
 
   ngOnInit() {
     this.titleService.setTitle("Pol | Update Installment");
+
+    this.retrievePolListing();
   }
+
+  showLOV() {
+    $('#polLovMdl > #modalBtn').trigger('click');
+  }
+
+  onRowClick(event){
+     if(Object.entries(event).length === 0 && event.constructor === Object){
+      this.selected = null;
+    } else {
+      this.selected = event;
+    }   
+  }
+
+  setDetails() {
+    console.log(this.selected);
+    if(this.selected != null) {
+        this.polNo = this.selected.policyNo.split('-');
+        this.policyId = this.selected.policyId;
+        this.cedingName = this.selected.cedComp;
+        this.insuredDesc = this.selected.insured;
+        this.riskName = this.selected.risk;
+    } else {
+      /*this.clearFields();*/
+    }
+  }
+
+  retrievePolListing(){
+       this.underwritingService.getParListing(this.searchParams).subscribe(data => {
+         var records = data['policyList'];
+          this.fetchedData = records;
+               for(let rec of records){
+                 console.log(rec);
+                   if(rec.altNo === 0){
+                      if (rec.statusDesc === 'In Force' || rec.statusDesc === 'In Progress' || rec.statusDesc === 'Approved' || rec.statusDesc === 'Pending Approval' || rec.statusDesc === 'Rejected') {
+                         this.passDataLOV.tableData.push(
+                                                    {
+                                                        policyId: rec.policyId,
+                                                        policyNo: rec.policyNo,
+                                                        cessionDesc: rec.cessionDesc,
+                                                        cedComp: rec.cedingName, 
+                                                        insured: rec.insuredDesc,
+                                                        risk: (rec.project == null) ? '' : rec.project.riskName,
+                                                        object: (rec.project == null) ? '' : rec.project.objectDesc,
+                                                        site: (rec.project == null) ? '' : rec.project.site,
+                                                        currency: rec.currencyCd,
+                                                        sumInsured: (rec.project.coverage == null) ? '' : rec.project.coverage.totalSi,
+                                                        premium: (rec.project.coverage == null) ? '' : rec.project.coverage.totalPrem,
+                                                        issueDate: this.ns.toDateTimeString(rec.issueDate),
+                                                        inceptDate: this.ns.toDateTimeString(rec.inceptDate),
+                                                        expiryDate: this.ns.toDateTimeString(rec.expiryDate),
+                                                        accDate: this.ns.toDateTimeString(rec.acctDate),
+                                                        status: rec.statusDesc
+                                                    }
+                                                );  
+                     }
+                   }
+               }
+                this.table.forEach(table => { table.refreshTable() });
+       });
+   }
 
 }
