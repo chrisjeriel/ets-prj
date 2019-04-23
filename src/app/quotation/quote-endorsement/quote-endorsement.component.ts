0f74cb8f13abdf1f0c27108514de86bb6fe37b05
Neco D. Caramonte <omegaxis19@gmail.com>
2019-04-23 17:00:46 +0800

    hotfix for create open cover and quote endorsements. Addresses the auto select first row of cust non datatable

--- src/app/quotation/quote-endorsement/quote-endorsement.component.ts
+++ src/app/quotation/quote-endorsement/quote-endorsement.component.ts
@@ -343,7 +343,7 @@ export class QuoteEndorsementComponent implements OnInit {
                          }
                        this.tableNonEditable.forEach(table => {table.refreshTable()});
                             this.table.refreshTable();
-                            this.optionTable.onRowClick(null,this.quoteOptionsData.tableData[0]);
+                            //this.optionTable.onRowClick(null,this.quoteOptionsData.tableData[0]);
                     });
                 }  
 
