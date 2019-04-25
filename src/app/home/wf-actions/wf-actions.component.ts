7b8d14ab08b058c1743f79b7e11b63d3b35f2fd1
etcarino <engelcarino@gmail.com>
2019-04-25 17:27:26 +0800

    savepoint 4/25/2019

--- src/app/home/wf-actions/wf-actions.component.ts
+++ src/app/home/wf-actions/wf-actions.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-wf-actions',
@@ -6,10 +7,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wf-actions.component.css']
 })
 export class WfActionsComponent implements OnInit {
+  @ViewChild('content') contentMdl : any;
 
-  constructor() { }
+  content: any ;
+  disablebtnBool: boolean;
+  disableAssignTo: boolean;
+  disableAssignToMany: boolean;
 
-  ngOnInit() {
-  }
+userInfo:any = {
+	userId: null,
+	userName: null
+}
+
+  constructor(config: NgbModalConfig,
+     private modalService: NgbModal) { }
+
+ngOnInit() {
+  	this.clear('disable');
+}
+
+open(content) {
+    this.content = content;
+    this.modalService.open(this.content, { centered: true , windowClass : 'modal-size'} );
+}
+
+handleRadioBtnChange(event){
+   	this.disablebtnBool = false;
+   	switch(event.target.value) { 
+   		case '1': { 
+        console.log('1'); 
+        this.clear('enable');
+        break; 
+        } 
+        case '2': { 
+        this.disableAssignTo = false;
+        $("a").removeClass('disabled-a').addClass('');
+        this.disableAssignToMany = true;
+        break; 
+        } 
+        case '3': { 
+        this.disableAssignTo = true;
+        $("a").removeClass('').addClass('disabled-a');
+        this.disableAssignToMany = false;
+        break; 
+        } 
+        case '4': { 
+        console.log('4'); 
+        this.clear('enable');
+        break;
+        } 
+         default: { 
+        //statements; 
+        break; 
+        } 
+    }
+}
+
+cancelReminder(){
+   	this.clear('disable');
+}
+
+clear(obj){
+	$("a").removeClass('').addClass('disabled-a');
+  	this.disableAssignTo = true;
+  	this.disableAssignToMany = true;
+
+  	if (obj == 'disable'){
+  		this.disablebtnBool = true;
+    } else {
+    	this.disablebtnBool = false;
+    }
+}
+
+showUsersLOV(){
+	$('#usersLOV #modalBtn').trigger('click');
+    $('#usersLOV #modalBtn').addClass('ng-dirty');
+}
+
+setPreparedBy(event){
+	console.log(event);
+
+	this.userInfo.userId = event.userId;
+	this.userInfo.userName = event.userName;
+
+	this.open(this.contentMdl);
+}
+
+blur(event){
+	$("a").removeClass('disabled-a').addClass('');
+}
 
 }
