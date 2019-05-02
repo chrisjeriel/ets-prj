c54a41a07adcbc9a8505fea327b9d3a5e08c555c
etcarino <engelcarino@gmail.com>
2019-05-02 08:40:56 +0800

    savepoint 5/2/2019

--- src/app/home/wf-actions/wf-actions.component.ts
+++ src/app/home/wf-actions/wf-actions.component.ts
@@ -5,6 +5,7 @@ import { MtnUsersComponent } from '@app/maintenance/mtn-users/mtn-users.componen
 import { CustNonDatatableComponent } from '@app/_components/common/cust-non-datatable/cust-non-datatable.component';
 import { SucessDialogComponent } from '@app/_components/common/sucess-dialog/sucess-dialog.component';
 import { finalize } from 'rxjs/operators';
+import { unHighlight, highlight, hideTooltip, showTooltip} from '@app/_directives/highlight';
 
 @Component({
   selector: 'app-wf-actions',
@@ -24,6 +25,12 @@ export class WfActionsComponent implements OnInit {
   reminderValue: any;
   dialogIcon:string  = "";
   dialogMessage:string  = "";
+  reminderDate: any;
+  alarmDate: any;
+  title: string = "";
+  reminder: string = "";
+  onOkVar: any;
+  disableAlarmTime: boolean = true;
 
   userInfo:any = {
 	userId: null,
@@ -130,15 +137,16 @@ cancel(){
 
 confirm(){
 	  this.selects = [];
-      for(var i = 0; i < this.usersListing.tableData.length; i++){
+    for(var i = 0; i < this.usersListing.tableData.length; i++){
         if(this.usersListing.tableData[i].checked){
           this.selects.push(this.usersListing.tableData[i]);
         }
-      }
+    }
 
     if (this.selects.length === 0 || this.selects.length === 1 ){
     	this.dialogIcon = "error-message";
         this.dialogMessage = "Please select at least 2 users";
+        this.onOkVar = "showUsersLOV";
         this.successDiag.open();
     }else {
     var records = this.selects;
@@ -149,10 +157,7 @@ confirm(){
 	this.userInfoToMany = temp;
 	this.open(this.contentMdl);
     }
-
-    
  }
-
 
 radioBtnChange(obj){
 	if (this.isEmptyObject(obj)){
@@ -249,7 +254,6 @@ showUsersLOV(obj){
 setPreparedBy(event){
 	this.userInfo.userId = event.userId;
 	this.userInfo.userName = event.userName;
-
     this.modalService.dismissAll();
 	this.open(this.contentMdl);
 }
@@ -270,6 +274,165 @@ checkCode(ev) {
     this.userInfo = [];
   	this.usersLov.checkCode(userId, ev);		
 }
+
+saveReminder(){
+	if (this.isEmptyObject(this.reminderDate) || this.isEmptyObject(this.reminder)){
+		this.openErrorDiag();
+	} else {
+		console.log(this.reminderValue);
+
+	    switch(this.reminderValue) { 
+	   		case '1': { 
+	   		
+	        break; 
+	        } 
+	        case '2': { 
+	          if (this.isEmptyObject(this.userInfo.userName)){
+			   this.openErrorDiag();
+			  } else {
+			 	console.log("save");
+			  }
+	        break; 
+	        } 
+	        case '3': { 
+	       	  if (this.isEmptyObject(this.userInfoToMany)){
+			   this.openErrorDiag();
+			  } else {
+			 	console.log("save");
+			  }
+	        break; 
+	        } 
+	        case '4': { 
+	        
+	        break;
+	        } 
+	         default: { 
+	        //statements; 
+	        break; 
+	        } 
+	    }
+
+    }
+}
+
+checkValidTime(event){
+	console.log(event.target.value);
+	if(this.isEmptyObject(event.target.value)){
+    } else {
+    	var reminderTime = this.toTimeString(Date.parse(this.reminderDate));	
+		if(this.isEmptyObject(this.alarmDate)){
+		} else {
+		  this.setAlarmTime(reminderTime,this.alarmDate);
+		}
+    }
+}
+
+
+checkValidDate(event){
+	if(this.isEmptyObject(event.target.value)){
+		this.disableAlarmTime = true;
+		this.alarmDate = null;
+	}else {
+		this.disableAlarmTime = false;
+		var reminderTime = this.toTimeString(Date.parse(this.reminderDate));	    
+
+		if(this.isEmptyObject(this.alarmDate)){
+		} else {
+		  this.setAlarmTime(reminderTime,this.alarmDate);
+		}
+
+	}
+}
+
+
+setAlarmTime(reminderTime: string, alarmTime: string){
+  var arrayTime = reminderTime.split(':');
+  var arrayAlarmTime = alarmTime.split(':');
+  var endHour = new Date();
+  var startHour = new Date();
+  var alarmHour = new Date();
+  startHour.setHours(0,0);
+  endHour.setHours(parseInt(arrayTime[0]),parseInt(arrayTime[1]));
+  alarmHour.setHours(parseInt(arrayAlarmTime[0]),parseInt(arrayAlarmTime[1]))
+
+  if (alarmHour >= startHour && alarmHour <= endHour){
+  } else {
+  	console.log("Choose in between");
+  }
+
+}
+
+
+
+toTimeString(millis: any) {
+    var d = (millis == 0) ? new Date() : new Date(millis);
+
+    function pad(num) {
+      return (num < 10) ? '0' + num : num;
+    }
+
+    return pad(d.getHours()) + ':' + pad(d.getMinutes());
+}
+
+openErrorDiag(){
+		this.dialogIcon = "error-message";
+        this.dialogMessage = "Please fill required fields";
+        this.onOkVar = "openReminderMdl";
+        this.successDiag.open();
+}
+
+onOkSuccessDiag(obj){
+	if(obj === 'showUsersLOV'){
+		this.showUsersLOV('0');
+	}else if (obj === 'openReminderMdl'){
+		console.log(this.alarmDate);
+		this.modalService.dismissAll();
+		this.open(this.content);
+	}
+}
+
+prepareParam(){
+    
+       var savePolGenInfoParam = {
+         "alarmTime"       : this.alarmDate,
+
+
+       }
+
+       console.log(savePolGenInfoParam);
+/*       this.loading = true;*/
+        
+
+ 
+}
+
+saveReminderParams(obj){
+
+	  this.us.updatePolGenInfo(obj).subscribe(data => {
+           console.log(data);
+        /*     this.loading = false;*/
+             if(data['returnCode'] === 0) {
+                this.dialogIcon = 'error';
+                this.dialogMessage = data['errorList'][0].errorMessage;
+                $('#updatePolGenInfo #successModalBtn').trigger('click');
+            } else if (data['returnCode'] === -1) {           
+                this.dialogIcon = 'success-message';
+                this.dialogMessage = "Successfully Saved";
+                this.policyInfo.updateUser = JSON.parse(window.localStorage.currentUser).username;
+                this.policyInfo.updateDate  = this.ns.toDateTimeString(0);
+                $('#updatePolGenInfo #successModalBtn').trigger('click');
+            }
+          });
+}
+
+
+
+
+
+
+
+
+
 
 
 }
