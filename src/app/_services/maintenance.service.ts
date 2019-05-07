787f07dc58b4f3c886894f69e8317bb6b7e5fe74
Neco D. Caramonte <omegaxis19@gmail.com>
2019-05-07 11:31:11 +0800

    updated maintenance risk listing according to FDD 05/06/2019

--- src/app/_services/maintenance.service.ts
+++ src/app/_services/maintenance.service.ts
@@ -139,19 +139,27 @@ export class MaintenanceService{
        	return this.http.get(environment.prodApiUrl + '/maintenance-service/retrieveMntIntermediary', {params});
     }
 
-	getMtnRiskListing(riskId,riskAbbr,riskName,regionDesc,provinceDesc,cityDesc,districtDesc,blockDesc,latitude,longitude,activeTag) {
-		const params = new HttpParams()
-                .set('riskId',riskId)
-                .set('riskAbbr',riskAbbr)
-                .set('riskName',riskName)
-                .set('regionDesc',regionDesc)
-                .set('provinceDesc',provinceDesc)
-                .set('cityDesc',cityDesc)
-                .set('districtDesc',districtDesc)
-                .set('blockDesc',blockDesc)
-                .set('latitude',latitude)
-                .set('longitude',longitude)
-                .set('activeTag',activeTag);
+	getMtnRiskListing(searchParams: any[]) {
+		var params;
+		if(searchParams.length < 1){
+			params = new HttpParams()
+	                .set('riskId','')
+	                .set('riskAbbr','')
+	                .set('riskName','')
+	                .set('regionDesc','')
+	                .set('provinceDesc','')
+	                .set('cityDesc','')
+	                .set('districtDesc','')
+	                .set('blockDesc','')
+	                .set('latitude','')
+	                .set('longitude','')
+	                .set('activeTag','');
+        }else{
+        	params = new HttpParams();
+            for(var i of searchParams){
+                params = params.append(i.key, i.search);
+            }
+        }
 
         return this.http.get(environment.prodApiUrl + '/maintenance-service/retrieveMtnRiskListing', {params});
 	}
