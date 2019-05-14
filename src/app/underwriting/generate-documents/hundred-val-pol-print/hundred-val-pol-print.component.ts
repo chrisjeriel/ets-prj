a7cab7d884c2a7057a0406ae28d532ea2996312e
Neco D. Caramonte <omegaxis19@gmail.com>
2019-05-14 15:05:39 +0800

    100% Value Policy Printing 1 of 2

--- src/app/underwriting/generate-documents/hundred-val-pol-print/hundred-val-pol-print.component.ts
+++ src/app/underwriting/generate-documents/hundred-val-pol-print/hundred-val-pol-print.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {  NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+import {  NgbTabChangeEvent, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NotesService, UnderwritingService } from '@app/_services';
+import { SucessDialogComponent } from '@app/_components/common/sucess-dialog/sucess-dialog.component';
+import { CancelButtonComponent } from '@app/_components/common/cancel-button/cancel-button.component';
+import { CustNonDatatableComponent } from '@app/_components/common/cust-non-datatable/cust-non-datatable.component';
 
 @Component({
   selector: 'app-hundred-val-pol-print',
@@ -9,9 +13,44 @@ import {  NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HundredValPolPrintComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,private router: Router) { }
+  @ViewChild(CancelButtonComponent) cancelBtn : CancelButtonComponent;
+  @ViewChild(SucessDialogComponent) successDiag: SucessDialogComponent;
+  @ViewChild(CustNonDatatableComponent) table: CustNonDatatableComponent;
+
+  dialogIcon: string = '';
+  dialogMessage: string = '';
+
+  selected: any;
+
+  btnDisabled: boolean = false;
+  isType: boolean = false;
+  noDataFound: boolean = false;
+
+  policyListingData: any = {
+  	tableData: [],
+  	tHeader: ['Policy No.', 'Ceding Company', 'Insured', 'Risk'],
+  	dataTypes: ['text', 'text', 'text', 'text'],
+  	pageLength: 10,
+  	pagination: true,
+  	pageStatus: true,
+  	keys: ['policyNo','cedingName', 'insuredDesc', 'riskName']
+  }
+
+  policyInfo: any = {
+  	policyId: '',
+  	policyNo: '',
+  	cedingName: '',
+  	insuredDesc: '',
+  	riskName: '',
+  	treatyPercent: ''
+  }
+
+  tempPolNo: string[] = ['','','','','',''];
+
+  constructor(private route: ActivatedRoute,private router: Router, private modalService: NgbModal, private us: UnderwritingService) { }
 
   ngOnInit() {
+  	this.retrievePolListing();
   }
 
   onTabChange($event: NgbTabChangeEvent) {            
@@ -20,5 +59,112 @@ export class HundredValPolPrintComponent implements OnInit {
         this.router.navigateByUrl('');
       }
     }
+
+	openPolListModal(){
+		this.retrievePolListing();
+		$('#lovMdl > #modalBtn').trigger('click');
+
+	}
+	retrievePolListing(){
+		this.us.getParListing([{key: 'policyNo', search: this.noDataFound ? '' : this.tempPolNo.join('%-%')}]).subscribe((data: any)=>{
+			console.log(data);
+			if(data.policyList.length !== 0){
+				this.noDataFound = false;
+				for(var rec of data.policyList){
+					this.policyListingData.tableData.push({
+						policyId: rec.policyId,
+						policyNo: rec.policyNo,
+						cedingName: rec.cedingName,
+						insuredDesc: rec.insuredDesc,
+						riskName: rec.project.riskName,
+						statusDesc: rec.statusDesc,
+						totalSi: rec.project.coverage.totalSi
+					});
+				}
+				this.policyListingData.tableData = this.policyListingData.tableData.filter(a => {return a.statusDesc.toUpperCase() !== 'SPOILED'});
+				this.table.refreshTable();
+				if(this.isType){
+					this.selected = this.policyListingData.tableData[0];
+					this.selectPol();
+					this.isType = false;
+				}
+			}else{
+				this.noDataFound = true;
+				this.isType = false;
+				setTimeout(()=>{this.openPolListModal()},100);
+			}
+		});
+	}
+
+	onRowClick(data){
+		if(data !== null && Object.keys(data).length !== 0){
+			this.selected = data;
+			this.btnDisabled = false;
+		}else{
+			this.btnDisabled = true;
+		}
+	}
+
+	selectPol(){
+		this.policyInfo.policyId = this.selected.policyId;
+		this.policyInfo.policyNo = this.selected.policyNo;
+		this.tempPolNo = this.selected.policyNo.split('-');
+		this.policyInfo.cedingName = this.selected.cedingName;
+		this.policyInfo.insuredDesc = this.selected.insuredDesc;
+		this.policyInfo.riskName = this.selected.riskName;
+	}
+
+	generate(){
+		this.us.getUWCoverageInfos(this.policyInfo.policyNo, this.policyInfo.policyId).subscribe((data:any)=>{
+			console.log(data);
+		});
+	}
+
+	checkPolicySearch(){
+		let emptyCheck: boolean = false;
+		for(var i of this.tempPolNo){
+			if(i.length === 0){
+				this.clearFields();
+				emptyCheck = true;
+				break;
+			}
+		}
+		if(!emptyCheck){
+			this.policyListingData.tableData = [];
+			this.isType = true;
+			this.retrievePolListing();
+		}
+	}
+
+	onClickCancel(){
+		this.cancelBtn.clickCancel();
+	}
+
+	clearFields(){
+		this.policyInfo = {
+						  	policyId: '',
+						  	policyNo: '',
+						  	cedingName: '',
+						  	insuredDesc: '',
+						  	riskName: '',
+						  	treatyPercent: ''
+						  };
+	}
+
+	pad(str, field) {
+	  if(str === '' || str == null){
+	    return '';
+	  }else{
+	    if(field === 'seqNo'){
+	      return String(str).padStart(5, '0');
+	    }else if(field === 'coSeriesNo'){
+	      return String(str).padStart(4, '0');
+	    }else if(field === 'cedingId'){
+	      return String(str).padStart(3, '0');
+	    }else if(field === 'altNo'){
+	      return String(str).padStart(2, '0');
+	    }
+	  }
+	}
 
 }
