5a3ea178ad3fdf2ef6ce82d11da1c890082804b3
Bryan <m.bryanmartinez@gmail.com>
2019-07-15 17:02:48 +0800

    savepoint

--- src/app/accounting-in-trust/reports/bordereaux/bordereaux.component.ts
+++ src/app/accounting-in-trust/reports/bordereaux/bordereaux.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { NotesService, MaintenanceService } from '@app/_services';
 import { MtnLineComponent } from '@app/maintenance/mtn-line/mtn-line.component';
+import { MtnTypeOfCessionComponent } from '@app/maintenance/mtn-type-of-cession/mtn-type-of-cession.component';
 
 @Component({
   selector: 'app-bordereaux',
@@ -12,9 +13,10 @@ import { MtnLineComponent } from '@app/maintenance/mtn-line/mtn-line.component';
 })
 export class BordereauxComponent implements OnInit {
 	@ViewChild('lineLov') lineLOV: MtnLineComponent;
+	@ViewChild('cessionLov') cessionLOV: MtnTypeOfCessionComponent;
 
 	dateParam: string = 'period';
-	extnType: string = 'outstanding';
+	extnType: string = 'O';
 	extnSubType: string = 'osLossDate';
 	clmHistType: string = 'loss';
 	perLine: boolean = true;
@@ -40,12 +42,39 @@ export class BordereauxComponent implements OnInit {
 		}
 	}
 
+	setExtnSubType() {
+		this.extnSubType = this.extnType == 'O' ? 'osLossDate' : 'lpTranDate';
+	}
+
 	showLineLOV() {
 		this.lineLOV.modal.openNoClose();
 	}
 
 	setLine(ev) {
+		this.ns.lovLoader(ev.ev, 0);
+
 		this.lineCd = ev.lineCd;
 		this.lineDesc = ev.description;
+	}
+
+	showCessionLOV() {
+		this.cessionLOV.modal.openNoClose();
+	}
+
+	setCession(ev) {
+		this.ns.lovLoader(ev.ev, 0);
+
+		this.cessionId = ev.cessionId;
+		this.cessionDesc = ev.description;
+	}
+
+	checkCode(ev, str) {
+		this.ns.lovLoader(ev, 1);
+
+		if(str == 'line') {
+			this.lineLOV.checkCode(this.lineCd, ev);
+		} else if(str == 'cession') {
+			this.cessionLOV.checkCode(this.cessionId, ev);
+		}
 	}
 }
