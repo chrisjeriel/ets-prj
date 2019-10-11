f9ac51f6e1b9d7dd03a1abe72bd1423e084cd818
EarlArcebido <33819853+EarlArcebido@users.noreply.github.com>
2019-10-11 17:45:56 +0800

    Series

--- src/app/maintenance/accounting-in-trust/generate-number-series/maint-jv-series-trst/maint-jv-series-trst.component.ts
+++ src/app/maintenance/accounting-in-trust/generate-number-series/maint-jv-series-trst/maint-jv-series-trst.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MaintenanceService, NotesService } from '@app/_services';
+import { SucessDialogComponent } from '@app/_components/common/sucess-dialog/sucess-dialog.component';
+import { ConfirmSaveComponent } from '@app/_components/common/confirm-save/confirm-save.component';
+import { CustEditableNonDatatableComponent } from '@app/_components/common/cust-editable-non-datatable/cust-editable-non-datatable.component'
 
 @Component({
   selector: 'app-maint-jv-series-trst',
@@ -7,22 +11,82 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaintJvSeriesTrstComponent implements OnInit {
   
+  @ViewChild(SucessDialogComponent) successDiag: SucessDialogComponent;
+  @ViewChild(ConfirmSaveComponent) confirm: ConfirmSaveComponent;
+  @ViewChild(CustEditableNonDatatableComponent) table: CustEditableNonDatatableComponent;
+
   passData: any = {
       tableData: [],
       tHeader: ['JV Year','JV No', 'Tran ID', 'Used','Created By', 'Date Created', 'Last Update By', 'Last Update'],
-      dataTypes: ['number','text', 'number', 'checkbox','text','date', 'text', 'date'],
+      dataTypes: ['year','text', 'number', 'checkbox','text','date', 'text', 'date'],
       searchFlag: true,
       infoFlag: true,
       paginateFlag: true,
       pageLength: 15,
       pageID: 1,
       uneditable: [true,true,true,true,true,true,true,true],
-      keys: ['quarterEnding', 'quarterEnding', 'currCd', 'currRate', 'balanceAmt', 'localAmt','balanceAmt', 'localAmt'],
+      keys: ['jvYear', 'jvNo', 'tranId', 'usedTag', 'createUser', 'createDate','updateUser', 'updateDate'],
   }
 
-  constructor() { }
+  params : any = {
+    jvFrom: '',
+    jvTo:'',
+    jvYear: '',
+    createUser: this.ns.getCurrentUser(),
+    createDate: this.ns.toDateTimeString(0),
+    updateUser: this.ns.getCurrentUser(),
+    updateDate: this.ns.toDateTimeString(0)
+  }
+
+  dialogMessage: string = "";
+  dialogIcon: string = "";
+  maxARSeries: number = 0;
+
+  constructor(private maintenanceService: MaintenanceService, private ns: NotesService) { }
 
   ngOnInit() {
+    this.retrieveMaxSeries();
   }
 
+  retrieveJVSeries(){
+    this.table.loadingFlag = true;
+    this.maintenanceService.getJvSeries(this.params.jvYear,this.params.jvFrom,this.params.jvTo).subscribe((data:any) => {
+      this.passData.tableData = [];
+      for (var i = 0; i < data.jvSeries.length; i++) {
+        this.passData.tableData.push(data.jvSeries[i]);
+      }
+      this.table.refreshTable();
+      this.table.loadingFlag = false;
+    });
+  }
+
+  onClickGenerate(){
+    if(this.params.jvFrom <= this.maxARSeries){
+      this.dialogMessage = "Existing number series was already created for the specified transaction numbers " + this.params.jvFrom + " to " + this.params.jvTo + ".Please adjust your From-To range values.";
+      this.dialogIcon = "error-message";
+      this.successDiag.open();
+    }else{
+      this.confirm.confirmModal();
+    }
+  }
+
+  generateJV(cancel?){
+    this.maintenanceService.generateJVSeries(this.params).subscribe((data:any) => {
+      if(data['returnCode'] != -1) {
+        this.dialogMessage = data['errorList'][0].errorMessage;
+        this.dialogIcon = "error";
+        this.successDiag.open();
+      }else{
+        this.dialogMessage = "";
+        this.dialogIcon = "success";
+        this.successDiag.open();
+      }
+    });
+  }
+
+  retrieveMaxSeries(){
+    this.maintenanceService.getMaxTranSeries('JV').subscribe((data:any) =>{
+      this.maxARSeries = data.maxTranNo.tranNo;
+    });
+  }
 }
