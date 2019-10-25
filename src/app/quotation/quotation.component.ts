285cd7e8272871a8fa35222d1415a9c80baba50d
Paul Remerata <paul26rem@gmail.com>
2019-10-25 15:11:21 +0800

    editing quotation from ready for printing change stat to in progress

--- src/app/quotation/quotation.component.ts
+++ src/app/quotation/quotation.component.ts
@@ -133,36 +133,31 @@ export class QuotationComponent implements OnInit {
 	onTabChange($event: NgbTabChangeEvent) {
 		 // if($('.ng-dirty:not([type="search"]):not(.not-form)').length != 0){
 		 // 	  $event.preventDefault();
-   //   }                   
-  		if ($event.nextId === 'approval-tab') {
-			$event.preventDefault();
-      this.showApprovalModal(this.content);
-		  }else
+   //   }   
+    if($('.ng-dirty.ng-touched:not([type="search"]):not(.exclude):not(.not-form)').length != 0 ){
+           $event.preventDefault();
+           const subject = new Subject<boolean>();
+           const modal = this.modalService.open(ConfirmLeaveComponent,{
+               centered: true, 
+               backdrop: 'static', 
+               windowClass : 'modal-size'
+           });
+           modal.componentInstance.subject = subject;
 
-      if ($event.nextId === 'Print') {
+           subject.subscribe(a=>{
+             if(a){
+               $('.ng-dirty').removeClass('ng-dirty');
+               this.tabset.select($event.nextId)
+             }
+           })
+      }else if ($event.nextId === 'approval-tab') {
+  			$event.preventDefault();
+        this.updateGenInfo();
+        this.showApprovalModal(this.content);
+		  }else if ($event.nextId === 'Print') {
         $event.preventDefault();
         $('#printListQuotation > #printModalBtn').trigger('click');
-      } else
-
-     if($('.ng-dirty.ng-touched:not([type="search"]):not(.exclude):not(.not-form)').length != 0 ){
-        $event.preventDefault();
-        const subject = new Subject<boolean>();
-        const modal = this.modalService.open(ConfirmLeaveComponent,{
-            centered: true, 
-            backdrop: 'static', 
-            windowClass : 'modal-size'
-        });
-        modal.componentInstance.subject = subject;
-
-        subject.subscribe(a=>{
-          if(a){
-            $('.ng-dirty').removeClass('ng-dirty');
-            this.tabset.select($event.nextId)
-          }
-        })
-
-
-      } else if ($event.nextId === 'Exit') {
+      }  else if ($event.nextId === 'Exit') {
         $event.preventDefault();
         this.router.navigateByUrl(this.exitLink);
       }
