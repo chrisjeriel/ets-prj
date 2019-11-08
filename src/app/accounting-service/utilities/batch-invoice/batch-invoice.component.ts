f1c4a48c44c5dab0f0ea30b8b2a5fb420e3789bf
PMMSC <PMMSC-ENGEL@cpi.local>
2019-11-08 09:10:36 +0800

    savepoint 11/8/2019

--- src/app/accounting-service/utilities/batch-invoice/batch-invoice.component.ts
+++ src/app/accounting-service/utilities/batch-invoice/batch-invoice.component.ts
@@ -10,20 +10,37 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./batch-invoice.component.css']
 })
 export class BatchInvoiceComponent implements OnInit {
+
+
+  dialogIcon: string = '';
+  dialogMessage: string = '';
+  jvTypes: any[] = [];
+  jvTypeCd : string;
+  jvNo : string;
+  fromDate: string = "";
+  toDate: string = "";
+  genInvoiceBool: boolean = true;
+  printInvoiceBool: boolean = false;
+  stopInvoiceBool: boolean = true;
+  boolSearch: boolean = false;
+  boolRadioTag: boolean = true;
+
   
   PassData: any = {
-  	tableData: this.accountingService.getBatchOR(),
+  	tableData: [],
   	tHeader: ['G', 'P', 'Invoice Date', 'Invoice No', 'Jv No.', 'JV Date','Billed To','Amount'],
   	dataTypes: ['checkbox','checkbox', 'date', 'text', 'text','date','text', 'currency'],
   	nData: new BatchOR(null,null,null,null,null,null,null,null),
     addFlag: true,
-    genericBtn: 'Cancel',
+    genericBtn: 'View Invoice Details',
   	searchFlag: true,
   	pageLength: 10,
     infoFlag: true,
     paginateFlag: true,
   	widths: [1,1,1,200,200,1,'auto',150],
-    uneditable: [true,true,true,true,true,true,true]
+    uneditable: [true,true,true,true,true,true,true],
+    pageID: 'invoiceBatchPrint',
+    disableGeneric: true
   }
 
   constructor(private accountingService: AccountingService, private modalService: NgbModal) { }
@@ -37,6 +54,12 @@ export class BatchInvoiceComponent implements OnInit {
 
   add(){
      $('#add, #modalBtn').trigger('click');
+  }
+
+  onTabChange($event: NgbTabChangeEvent) {
+    if ($event.nextId === 'Exit') {
+        this.router.navigateByUrl('');
+      } 
   }
   
 
