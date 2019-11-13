4df214ec81940dfe6f82b1cc53136b9087d2d39f
Paul Remerata <paul26rem@gmail.com>
2019-11-13 15:38:05 +0800

    wip update quote attachment

--- src/app/quotation/utilities/update-quote-attachment/update-quote-attachment.component.ts
+++ src/app/quotation/utilities/update-quote-attachment/update-quote-attachment.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { LoadingTableComponent } from '@app/_components/loading-table/loading-table.component';
 import { ConfirmLeaveComponent,CancelButtonComponent, CustEditableNonDatatableComponent,ConfirmSaveComponent, ModalComponent, SucessDialogComponent } from '@app/_components/common';
 import { NgForm } from '@angular/forms';
-import { QuotationService, NotesService} from '@app/_services';
+import { QuotationService, NotesService, UploadService } from '@app/_services';
+import {HttpClient, HttpParams, HttpRequest, HttpEvent, HttpEventType, HttpResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-update-quote-attachment',
@@ -12,7 +13,7 @@ import { QuotationService, NotesService} from '@app/_services';
 export class UpdateQuoteAttachmentComponent implements OnInit {
 
   quoteInfo:any = {
-  	quotationNo:[]
+  	quotationNoArr:[]
 
   };
 
@@ -43,38 +44,74 @@ export class UpdateQuoteAttachmentComponent implements OnInit {
 
   completeSearch:boolean = false;
 
+  passData : any = {
+    tableData: [],
+    dataTypes: ['text', 'text'],
+    tHeader: ['File Name', 'Description', 'Action'],
+    nData :{
+    	fileName:'',
+    	description:''
+    },
+    checkFlag: true,
+    selectFlag: false,
+    addFlag: true,
+    editFlag: false,
+    deleteFlag: true,
+    paginateFlag: true,
+    infoFlag: true,
+    searchFlag: true,
+    checkboxFlag: true,
+    pageLength: 10,
+    widths: [],
+    uneditable: [true,false,false],
+    keys:['fileName','description'],
+    disableAdd:true
+  };
+
   @ViewChild('quoteMdl') quoteMdl: ModalComponent;
   @ViewChild(LoadingTableComponent) quoteTable: LoadingTableComponent;
+  @ViewChild(CustEditableNonDatatableComponent) table : CustEditableNonDatatableComponent;
 
-  constructor(private qs: QuotationService) { } 
+  filesList: any[] = [];
+
+  dialogMessage:string;
+  dialogIcon:string;
+  @ViewChild(SucessDialogComponent) successDiag :SucessDialogComponent; 
+  @ViewChild(ConfirmSaveComponent) confirmSave: ConfirmSaveComponent;
+
+  cancelFlag:boolean;
+
+  constructor(private qs: QuotationService, private upload: UploadService, private ns: NotesService) { } 
 
   ngOnInit() {
   }
 
   search(key,ev) {
-  		this.quoteInfo.quotationNo[0] =  (this.quoteInfo.quotationNo[0] == undefined || this.quoteInfo.quotationNo[0] == '')?'':this.quoteInfo.quotationNo[0];
-  		this.quoteInfo.quotationNo[1] =  (this.quoteInfo.quotationNo[1] == undefined || this.quoteInfo.quotationNo[1] == '')?'':this.quoteInfo.quotationNo[1];
-		this.quoteInfo.quotationNo[2] =  (this.quoteInfo.quotationNo[2] == undefined || this.quoteInfo.quotationNo[2] == '')?'':this.quoteInfo.quotationNo[2].padStart(5,'0');
-		this.quoteInfo.quotationNo[3] =  (this.quoteInfo.quotationNo[3] == undefined || this.quoteInfo.quotationNo[3] == '')?'':this.quoteInfo.quotationNo[3].padStart(2,'0');
-		this.quoteInfo.quotationNo[4] =  (this.quoteInfo.quotationNo[4] == undefined || this.quoteInfo.quotationNo[4] == '')?'':this.quoteInfo.quotationNo[4].padStart(3,'0');
+  		this.quoteInfo.quotationNoArr[0] =  (this.quoteInfo.quotationNoArr[0] == undefined || this.quoteInfo.quotationNoArr[0] == '')?'':this.quoteInfo.quotationNoArr[0];
+  		this.quoteInfo.quotationNoArr[1] =  (this.quoteInfo.quotationNoArr[1] == undefined || this.quoteInfo.quotationNoArr[1] == '')?'':this.quoteInfo.quotationNoArr[1];
+		this.quoteInfo.quotationNoArr[2] =  (this.quoteInfo.quotationNoArr[2] == undefined || this.quoteInfo.quotationNoArr[2] == '')?'':this.quoteInfo.quotationNoArr[2].padStart(5,'0');
+		this.quoteInfo.quotationNoArr[3] =  (this.quoteInfo.quotationNoArr[3] == undefined || this.quoteInfo.quotationNoArr[3] == '')?'':this.quoteInfo.quotationNoArr[3].padStart(2,'0');
+		this.quoteInfo.quotationNoArr[4] =  (this.quoteInfo.quotationNoArr[4] == undefined || this.quoteInfo.quotationNoArr[4] == '')?'':this.quoteInfo.quotationNoArr[4].padStart(3,'0');
 		
-		
-		if(this.quoteInfo.quotationNo.includes('')) {
+		this.searchParams.quotationNo = this.quoteInfo.quotationNoArr.map(a=>a.toUpperCase()).join('%-%');
+		this.passDataQuoteLOV.filters[0].search = this.searchParams.quotationNo;
+    	this.passDataQuoteLOV.filters[0].enabled =true;
+
+		if(this.quoteInfo.quotationNoArr.includes('')) {
 			//this.searchArr = this.searchArr.map(a => { a = a === '' ? '%%' : a; return a; });
 			this.completeSearch = false;
 			this.quoteInfo  = {
-				quotationNo 	: this.quoteInfo.quotationNo,
+				quotationNo 	: '',
+				quotationNoArr  : this.quoteInfo.quotationNoArr,
 				cedingName		: '',
 				insuredDesc		: '',
 				riskName		: '',
 				totalSi			: '',
 				status			: ''
 			};
-			this.searchParams.quotationNo = this.quoteInfo.quotationNo.join('%-%');
 		}else{
-			this.searchParams.quotationNo = this.quoteInfo.quotationNo.join('%-%');
-			this.passDataQuoteLOV.filters[0].search = this.searchParams.quotationNo;
-	    	this.passDataQuoteLOV.filters[0].enabled =true;
+			this.completeSearch = true;
+			
 			this.getQuoteList();
 		}
 
@@ -86,8 +123,25 @@ export class UpdateQuoteAttachmentComponent implements OnInit {
 	getQuoteList(){
 		this.qs.newGetQuoProcessingData(this.searchParams).subscribe(a=>{
 			this.passDataQuoteLOV.count = a['length'];
-			this.quoteTable.placeData(a['quotationList']); 
-
+			this.quoteTable.placeData(a['quotationList'].map(i => 
+					{ 
+						i.riskName = (i.project == null || i.project == undefined)?'':i.project.riskName;
+					  	return i;
+					}
+				)
+			); 
+			if(a['quotationList'].length == 1 && this.completeSearch){
+				this.quoteInfo = a['quotationList'][0];
+				this.quoteInfo.quotationNoArr = this.quoteInfo.quotationNo.split('-');
+				this.getAttachment();
+			}else if(a['quotationList'].length == 0 && this.completeSearch){
+				this.searchParams = {
+										'paginationRequest.count':10,
+				        				'paginationRequest.position':1,   
+				        			};
+    			this.completeSearch = false;
+    			this.showQuoteLov();
+			}
 		});
 	}
 
@@ -95,4 +149,194 @@ export class UpdateQuoteAttachmentComponent implements OnInit {
 		this.quoteMdl.openNoClose();
 		this.getQuoteList();
 	}
+
+	searchQuery(searchParams){
+		for(let key of Object.keys(searchParams)){
+            this.searchParams[key] = searchParams[key]
+        }
+        this.completeSearch = false;
+		this.getQuoteList();
+	}
+
+	onClickOkQuoteLov(){
+		this.quoteInfo = this.quoteTable.indvSelect;
+		this.quoteInfo.quotationNoArr = this.quoteInfo.quotationNo.split('-');
+		this.quoteMdl.closeModal();
+		this.getAttachment();
+		console.log(this.quoteInfo)
+	}
+
+
+	// ATTACHMENT LAND
+
+  getAttachment(){
+    this.passData.tableData = [];
+    this.passData.disableAdd = false;
+    this.qs.getAttachment(this.quoteInfo.quoteId, null)
+      .subscribe(data => {
+        this.passData.tableData = data['quotation'][0].attachmentsList.map(i=>{
+        	i.fileNameServer = i.fileName;
+        	i.module = 'quotation';
+        	i.refId = this.quoteInfo.quoteId;
+        	return i;
+        });
+    	this.table.refreshTable();
+  	});
+  }
+
+  uploads(event){
+	  this.filesList = event;
+  }
+
+  onClickSave(){
+    this.filesList = this.filesList.filter(a=>{return this.passData.tableData.map(a=>{return a.fileName}).includes(a[0].name)});
+      if(!this.checkFields()){
+        this.dialogMessage="";
+        this.dialogIcon = "error";
+        this.successDiag.open();
+      }else if(this.checkFileSize().length !== 0){
+        this.dialogMessage= this.checkFileSize()+" exceeded the maximum file upload size.";
+        this.dialogIcon = "error-message";
+        this.successDiag.open();
+      }else if(this.checkFileNameLength()){
+        this.dialogMessage= "File name exceeded the maximum 250 characters";
+        this.dialogIcon = "error-message";
+        this.successDiag.open();
+      }else{
+        this.confirmSave.confirmModal();
+      }
+  }
+
+  checkFields(){
+      for(let check of this.passData.tableData){
+        if(check.description === null || check.description === undefined || check.description.length === 0 ||
+           check.fileName === null || check.fileName === null || check.fileName.length === 0){
+          return false;
+        }
+      }
+      return true;
+   }
+
+   checkFileSize(){
+     for(let files of this.filesList){
+       console.log(files[0].size);
+       if(files[0].size > 26214400){ //check if a file exceeded 25MB
+         return files[0].name;
+       }
+     }
+     return '';
+   }
+
+   checkFileNameLength(){
+     for(var i of this.passData.tableData){
+       if(i.fileName.length > 250){
+         return true;
+       }
+     }
+     return false;
+   }
+
+    onSaveAttachment(cancelFlag?){
+      this.dialogIcon = '';
+      this.dialogMessage = '';
+      this.cancelFlag = cancelFlag !== undefined;
+      let savedData = [];
+      let deletedData = [];
+      for (var i = 0 ; this.passData.tableData.length > i; i++) {
+        if(this.passData.tableData[i].edited && !this.passData.tableData[i].deleted){
+            savedData.push(this.passData.tableData[i]);
+            savedData[savedData.length-1].createDate = this.ns.toDateTimeString(0);
+            savedData[savedData.length-1].createUser = JSON.parse(window.localStorage.currentUser).username;
+            savedData[savedData.length-1].updateDate = this.ns.toDateTimeString(0);
+            savedData[savedData.length-1].updateUser = JSON.parse(window.localStorage.currentUser).username;
+        }
+        else if(this.passData.tableData[i].edited && this.passData.tableData[i].deleted){
+           deletedData.push(this.passData.tableData[i]);
+           deletedData[deletedData.length-1].createDate = this.ns.toDateTimeString(0);
+           deletedData[deletedData.length-1].updateDate = this.ns.toDateTimeString(0);
+        }
+
+      }
+
+      let params: any = {
+        deleteAttachmentsList: deletedData,
+        saveAttachmentsList: savedData,
+        quoteId: this.quoteInfo.quoteId
+      }
+
+      this.qs.saveQuoteAttachment(JSON.stringify(params))
+        .subscribe((data: any) => {
+          console.log(data);
+          if(data.returnCode === 0){
+            this.cancelFlag = false;
+            this.dialogIcon = 'error';
+            this.successDiag.open();
+            console.log(this.cancelFlag);
+          }else{
+            this.dialogIcon = 'success';
+            this.successDiag.open();
+            if(data.uploadDate != null){
+              this.uploadMethod(data.uploadDate);
+            }
+            if(deletedData.length !== 0){
+              this.deleteFileMethod();
+            }
+            this.getAttachment();
+            this.table.markAsPristine();
+          }
+          this.loading = false;
+        });
+    } 
+
+    uploadMethod(date){
+      //upload
+      for(let files of this.filesList){
+        if (files.length == 0) {
+          console.log("No file selected!");
+          return
+
+        }
+        let file: File = files[0];
+        //var newFile = new File([file], date + file.name, {type: file.type});
+        this.upload.uploadFile(file, date, 'quotation', this.quoteInfo.quoteId)
+          .subscribe(
+            event => {
+              if (event.type == HttpEventType.UploadProgress) {
+                const percentDone = Math.round(100 * event.loaded / event.total);
+                console.log(`File is ${percentDone}% loaded.`);
+              } else if (event instanceof HttpResponse) {
+                console.log('File is completely loaded!');
+              }
+            },
+            (err) => {
+              console.log("Upload Error:", err);
+            }, () => {
+              console.log("Upload done");
+            }
+          )
+        }
+        //clear filelist array after upload
+        this.table.filesToUpload = [];
+        this.table.refreshTable();
+    }
+
+    deleteFileMethod(){
+     let deleteFile = this.deletedData;
+     for(var i of deleteFile){
+       console.log(i.fileNameServer);
+       this.upload.deleteFile(i.fileNameServer, 'quotation', this.quoteInfo.quoteId).subscribe(
+           data =>{
+             console.log(data);
+           },
+           error =>{
+             console.log('Error: '+ error);
+           },
+           () =>{
+             console.log('Successfully deleted');
+           }
+         );
+     }
+   }
+
+
 }
