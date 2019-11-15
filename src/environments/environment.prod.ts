c28d83e8f675e3864028026b761b4fbd8f10a871
CPI Projects <cpiprojects@cpi.com.ph>
2019-11-15 16:07:37 +0800

    Merged with PCPR/EJVA.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'http://192.10.10.210:8890/api',
   successMessage: "Successfully Saved!",
-  /*prodApiUrl: 'http://192.168.99.163:8890/api'*/
+  /*prodApiUrl: 'http://192.168.99.163:8888/api'*/
+  /*http://192.168.99.201:8888/api*/
 
   prodApiUrl: 'http://192.10.10.210:8890/api'
 };
