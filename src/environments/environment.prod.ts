5991108972d1c8ae2c8fd70a68c0f9018f8e5f19
CPI Projects <cpiprojects@cpi.com.ph>
2019-11-19 09:43:38 +0800

    Commited for 1st User Training.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: 'http://192.10.10.210:8890/api',
   successMessage: "Successfully Saved!",
   /*prodApiUrl: 'http://192.168.99.163:8888/api'*/
-  /*http://192.168.99.201:8888/api*/
+  /*http://192.168.99.202:8888/api*/
 
   prodApiUrl: 'http://192.10.10.210:8890/api'
 };
