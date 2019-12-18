d83c4af51138c9c414244d62d933305a801e5271
CPI Projects <cpiprojects@cpi.com.ph>
2019-12-18 15:43:02 +0800

    Registers reports for ACIT.

---
+++ src/app/claims/reports/claim-reports/claim-reports.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-reports',
+  templateUrl: './claim-reports.component.html',
+  styleUrls: ['./claim-reports.component.css']
+})
+export class ClaimReportsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
