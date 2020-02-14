d3c833669088fbeaeaa33604d4add799266277c5
bryan <mmmartinez@cpi.com.ph>
2020-02-14 10:01:29 +0800

    pol-brdrx

--- src/app/accounting-in-trust/reports/pol-bordereaux/pol-bordereaux.component.ts
+++ src/app/accounting-in-trust/reports/pol-bordereaux/pol-bordereaux.component.ts
@@ -73,7 +73,7 @@ export class PolBordereauxComponent implements OnInit {
     forceExtract: 'N',
   };
 
-  repExtractions: Array<string> = [
+  /*repExtractions: Array<string> = [
                         'POLR044A',
                         'POLR044B',
                         'POLR044C',
@@ -100,6 +100,13 @@ export class PolBordereauxComponent implements OnInit {
                         'ACITR061F',
                         'ACITR061G',
                         'ACITR048A'
+                        ];*/
+
+  repExtractions: Array<string> = [
+                        'POLR052A',
+                        'POLR052B',
+                        'POLR052C',
+                        'POLR052D'
                         ];
 
   rangeParams :any = {
@@ -142,7 +149,255 @@ export class PolBordereauxComponent implements OnInit {
   constructor(private ms: MaintenanceService, private ns: NotesService, private printService: PrintService, public modalService: NgbModal,  private decimal : DecimalPipe, private router:Router) { }
 
   ngOnInit() {
-  	 this.loading = false;
+    this.passLov.modReportId = 'POLR052%';
+  	this.loading = false;
+  }
+
+  getReports(){
+    this.passLov.reportId = 'POLR052%';
+    this.lovMdl.openLOV();
+  }
+
+  setReport(data){
+    setTimeout(()=>{
+      $('.reports').focus().blur();
+    },0);
+    this.paramsToggle = [];
+    this.params = [];
+    this.params.effDate = this.ns.toDateTimeString(0);
+
+    if(data.data != null){
+      this.params.reportId = data.data.reportId;
+      this.params.reportName = data.data.reportTitle;
+    }
+
+    if (this.repExtractions.indexOf(this.params.reportId) > -1) {
+      this.extractDisabled = false;
+    } else {
+      this.extractDisabled = true;
+    }
+
+    if(this.params.reportId == 'POLR052A'){
+      this.paramsToggle.push('accountingDate', 'line', 'company', 'byMonthYear', 'currCd');
+      this.params.dateParam = '5';
+      this.params.dateRange = '2';
+      this.checkMonthYear();
+    } 
+    else if(this.params.reportId == 'POLR052B'){
+      this.paramsToggle.push('accountingDate', 'line', 'company', 'byMonthYear', 'currCd');
+      this.params.dateParam = '5';
+      this.params.dateRange = '2';
+      this.checkMonthYear();
+    }
+    else if(this.params.reportId == 'POLR052C'){
+      this.paramsToggle.push('accountingDate', 'line', 'company', 'byMonthYear', 'currCd');
+      this.params.dateParam = '5';
+      this.params.dateRange = '2';
+      this.checkMonthYear();
+    }
+    else if(this.params.reportId == 'POLR052D'){
+      this.paramsToggle.push('accountingDate', 'line', 'company', 'byDate', 'byMonthYear', 'currCd');
+      this.params.dateParam = '5';
+    }
+
+    this.ns.lovLoader(data.ev, 0);
+  }
+
+  checkReport(){
+
+  }
+
+  checkMonthYear(){
+
+  }
+
+  removeDates(){
+    this.params.byDateFrom = '';
+    this.params.byDateTo = '';
+    this.params.byMonthFrom = '';
+    this.params.byMonthTo = '';
+    this.params.byMonthToYear = '';
+    this.params.byMonthFromYear = '';
+    this.params.byAsOf = '';
+  }
+
+  getLine(){
+    this.lineLov.modal.openNoClose();
+  }
+  
+  setLine(data){
+    this.params.lineCd = data.lineCd;
+    this.params.lineName = data.description;
+    this.ns.lovLoader(data.ev, 0);
+  }
+
+  showCedingCompanyLOV() {
+    this.cedingLov.modal.openNoClose();
+  }
+
+  setCedingcompany(data){
+    this.params.cedingId = data.cedingId;
+    this.params.cedingName = data.cedingName; 
+    this.ns.lovLoader(data.ev, 0);
+  }
+
+  prepareData(){
+    this.modalMode = "";
+
+    if(this.params.dateRange == 1){
+      this.sendData.fromDate = this.ns.toDateTimeString(this.params.byDateFrom);
+      this.sendData.toDate = this.ns.toDateTimeString(this.params.byDateTo);
+    }else if(this.params.dateRange == 2){
+      this.sendData.fromDate = this.ns.toDateTimeString(new Date(this.params.byMonthFromYear,this.params.byMonthFrom,0).getTime());
+      this.sendData.toDate = this.ns.toDateTimeString(new Date(this.params.byMonthToYear,this.params.byMonthTo,0).getTime());
+    }else if(this.params.dateRange == 3){
+      this.sendData.toDate = this.ns.toDateTimeString(this.params.byAsOf);
+    }
+
+    this.sendData.dateRange = this.params.dateRange == 1 ? "D" : (this.params.dateRange == 2 ? "M" : "A");
+    this.sendData.reportId = this.params.reportId;
+    this.sendData.dateParam = this.params.dateParam;
+    this.sendData.lineCdParam = this.params.lineCd;
+    this.sendData.currCdParam = this.params.currCd;
+    this.sendData.cedingIdParam = this.params.cedingId;
+    this.sendData.incRecTag = 'D'; //this.params.incRecTag;
+    this.sendData.destination = this.params.destination;
+  }
+
+  extract(cancel?){
+    this.tableFlag = true;
+    if(this.params.dateRange !== ''){
+      if(this.params.dateRange == 1 && (this.params.byDateFrom == '' || this.params.byDateFrom == undefined) && 
+         (this.params.byDateTo == '' || this.params.byDateTo == undefined)){
+        this.dialogIcon = "warning-message";
+        this.dialogMessage = "Please select dates to be extracted";
+        this.appDialog.open();
+        return;
+      }else if(this.params.dateRange == 2 && ((this.params.byMonthFromYear == '' || this.params.byMonthFromYear == undefined) ||
+               (this.params.byMonthFrom == '' || this.params.byMonthFrom == undefined)) && ((this.params.byMonthToYear == '' ||this.params.byMonthToYear == undefined) ||
+               (this.params.byMonthTo == '' || this.params.byMonthTo == undefined))){
+        this.dialogIcon = "warning-message";
+        this.dialogMessage = "Please select dates to be extracted";
+        this.appDialog.open();
+        return;
+      }else if(this.params.dateRange == 3 && (this.params.byAsOf == '' || this.params.byAsOf == undefined)){
+        this.dialogIcon = "warning-message";
+        this.dialogMessage = "Please select dates to be extracted";
+        this.appDialog.open();
+        return;
+      }else if(this.params.dateRange == undefined){
+        this.dialogIcon = "warning-message";  
+        this.dialogMessage = "Please select dates to be extracted";
+        this.appDialog.open();
+        return;
+      }
+    }
+
+    this.loading = true;
+    this.prepareData();
+    console.log(this.params.reportId);
+    console.log(this.sendData);
+
+    /*this.printService.extractReport({ reportId: this.params.reportId, polr044Params:this.sendData }).subscribe((data:any)=>{
+        console.log("extractReport return data");
+        console.log(data);
+        this.loading = false;
+        if (data.errorList.length > 0) {
+          
+          if (data.errorList[0].errorMessage.includes("parameters already exists.")) {
+            this.modalMode = "reExtract";
+            this.modalHeader = "Confirmation";
+            this.polReportsModal.openNoClose();
+          } else {
+            this.dialogIcon = 'error';
+            this.appDialog.open();
+          }
+          
+        } else {
+          if (data.params.extractCount != 0) {
+            this.modalHeader = "Extraction Completed";
+            this.modalBody = "Successfully extracted " + data.params.extractCount + " record/s.";
+            this.polReportsModal.openNoClose();
+          } else {
+            this.modalHeader = "Extraction Completed";
+            this.modalBody = "No record/s extracted.";
+            this.polReportsModal.openNoClose();
+          }
+        }
+    },
+    (err) => {
+      alert("Exception when calling services.");
+    });*/
+    
+    this.sendData.forceExtract = 'N';
+  }
+
+  forceExtract() {
+    this.sendData.forceExtract = 'Y';
+  }
+
+  print() {
+    this.tableFlag = true;
+    if(this.params.destination === '' || this.params.destination === undefined){
+      this.dialogIcon = "warning-message";
+      this.dialogMessage = "Please select a print destination";
+      this.appDialog.open();
+      return;
+    }
+
+    this.prepareData();
+
+    let params :any = {
+      "reportId" : this.params.reportId,
+      "polr044Params.extractUser" :   this.sendData.extractUser,
+      "polr044Params.dateRange" :   this.sendData.dateRange,
+      "polr044Params.dateParam" :  this.sendData.dateParam,
+      "polr044Params.fromDate"  :   this.sendData.fromDate,
+      "polr044Params.toDate"    :     this.sendData.toDate,
+      "polr044Params.cedingIdParam" : this.sendData.cedingIdParam,
+      "polr044Params.lineCdParam" :   this.sendData.lineCdParam,
+      "polr044Params.incRecTag" :   this.sendData.incRecTag,
+      "polr044Params.reportId"  :   this.sendData.reportId,
+    }
+
+    this.printService.print(this.params.destination,this.params.reportId, params);
+  }
+
+  onClickCancel(){
+    this.router.navigateByUrl('');
+  }
+
+  checkCode(ev, field){
+    this.ns.lovLoader(ev, 1);
+
+    if(field === 'line') {            
+        this.lineLov.checkCode(this.params.lineCd, ev);
+    } else if(field === 'company') {
+        this.cedingLov.checkCode(String(this.params.cedingId).padStart(3, '0'), ev);            
+    } else if(field === 'report'){
+      if(this.params.reportId.indexOf('POLR052') == -1){
+        this.passLov.code = 'POLR052%';
+      }else{
+        this.passLov.code = this.params.reportId;
+      }
+      this.lovMdl.checkCode('reportId',ev);
+    }else if(field == 'currCd') {
+      this.currLov.checkCode(this.params.currCd, ev);
+    }
+  }
+
+  showCurrencyModal(){
+    $('#currencyModal #modalBtn').trigger('click');
+  }
+  
+  setCurrency(data){
+    console.log(data)
+    this.params.currCd = data.currencyCd;
+    this.params.currdesc = data.description;
+    this.ns.lovLoader(data.ev, 0);
+    setTimeout(()=>{
+          $('.currCd').focus().blur();
+        }, 0);
   }
 
 }
