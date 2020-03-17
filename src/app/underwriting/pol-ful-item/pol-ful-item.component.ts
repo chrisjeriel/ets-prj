4a168ec7a4c728fd511325c19d157adbffa7fa96
paul26rem <paul26rem@gmail.com>
2020-03-17 16:25:22 +0800

    init full item

---
+++ src/app/underwriting/pol-ful-item/pol-ful-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pol-ful-item',
+  templateUrl: './pol-ful-item.component.html',
+  styleUrls: ['./pol-ful-item.component.css']
+})
+export class PolFulItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
