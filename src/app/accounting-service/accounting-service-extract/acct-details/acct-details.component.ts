7fd47d840acd712c0c1c7f3f702f257ebd247bb9
bryan <mmmartinez@cpi.com.ph>
2020-11-25 15:06:28 +0800

    bir taxes

--- src/app/accounting-service/accounting-service-extract/acct-details/acct-details.component.ts
+++ src/app/accounting-service/accounting-service-extract/acct-details/acct-details.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { AccountingEntriesExtract, CredibleWithholdingTaxDetails ,InputVatDetails, OutputVatDetails, WithholdingVATDetails } from '@app/_models';
-import { AccountingService } from '@app/_services';
+import { AccountingService, MaintenanceService, NotesService } from '@app/_services';
+import { CustEditableNonDatatableComponent } from '@app/_components/common/cust-editable-non-datatable/cust-editable-non-datatable.component'
 
 @Component({
   selector: 'app-acct-details',
@@ -9,262 +10,53 @@ import { AccountingService } from '@app/_services';
 })
 export class AcctDetailsComponent implements OnInit {
   @Input() taxType: string = "";
+  @ViewChild('table') table: CustEditableNonDatatableComponent;
 
-  CreditableTaxData: any = {
-    tableData: this.accountingService.getCredibleWithholdingTaxDetails(),
-    tHeader: ["Ref. No.","Payee/Payor","BIR Code","Tax Description","TIN","TIN Branch","Income","Whtax Rate","Whtax AMount"],
-    dataTypes: ["text","text","text","text","text","text","text","currency","currency"],
-    resizable: [true, true, true, true, true, true,true,true,true],
-    nData: new CredibleWithholdingTaxDetails(null,null,null,null,null,null,null,null,null),
+  passData: any = {
+    tableData: [],
+    tHeader: ["Transaction No.","Transaction Date","Company Name","TIN","Address","Base Amount","Tax Amount","Tax Code","Tax Rate"],
+    dataTypes: ["text","date","text","text","text","currency","currency","text","percent"],
+    keys: ['tranNo','tranDate','payeeName','tin','mailAddress','baseAmt','taxAmt','taxCd','rate'],
     pageLength: 15,
-    widths: [150,220,150,150,150,150,150,150,150],
-    total:[null,null,null,null,null,'Total','income',null,'whtaxAmount'],
-    paginateFlag:true,
-    infoFlag:true,
-    searchFlag:true,
-    filters: [
-      {
-        key: 'refNo',
-        title: 'Ref. No.',
-        dataType: 'text'
-      },
-      {
-        key: 'payeePayor',
-        title: 'Payee/Payor',
-        dataType: 'text'
-      },
-      {
-        key: 'birCode',
-        title: 'BIR Code',
-        dataType: 'text'
-      },
-      {
-        key: 'taxDescription',
-        title: 'Tax Description',
-        dataType: 'text'
-      },
-      {
-        key: 'tin',
-        title: 'TIN',
-        dataType: 'text'
-      },
-      {
-        key: 'tinBranch',
-        title: 'TIN Branch',
-        dataType: 'text'
-      },
-      {
-        key: 'income',
-        title: 'Income',
-        dataType: 'text'
-      },
-      {
-        key: 'whtaxRate',
-        title: 'Whtax Rate',
-        dataType: 'text'
-      },
-      {
-        key: 'whtaxAmount',
-        title: 'Whtax Amount',
-        dataType: 'text'
-      }
-    ]
-
+    // widths: [150,220,150,150,150,150,150,150,150],
+    uneditable: [true,true,true,true,true,true,true,true,true],
+    total:[null,null,null,null,'Total',null,'taxAmt',null,null],
+    paginateFlag: true,
+    infoFlag: true,
+    searchFlag: true
   }
 
-  
-  InputVatDetailsData: any = {
-    tableData: this.accountingService.getInputVatDetails(),
-    tHeader: ["Tran Type","Ref. No.","Payee/Payor", "Address 1", "Address 2", "TIN", "BIR RLF Purchase Type","Base Amount","VAT Rate","Input VAT Amount"],
-    dataTypes: ["text","text","text","text","text","text","text","currency","currency","currency"],
-    resizable: [true, true, true, true, true, true,true,true,true,true],
-    nData: new InputVatDetails(null,null,null,null,null,null,null,null,null,null),
-    pageLength: 15,
-    widths: [150,220,150,150,150,150,150,150,150,150],
-    total:[null,null,null,null,null,null,'Total','baseAmount',null,'inputVatAmount'],
-    paginateFlag:true,
-    infoFlag:true,
-    searchFlag:true,
-    filters: [
-      {
-        key: 'tranType',
-        title: 'Tran Type:',
-        dataType: 'text'
-      },
-      {
-        key: 'refNo',
-        title: 'Ref. No.',
-        dataType: 'text'
-      },
-      {
-        key: 'payeePayor',
-        title: 'Payee/Payor',
-        dataType: 'text'
-      },
-      {
-        key: 'address1',
-        title: 'Address 1',
-        dataType: 'text'
-      },
-      {
-        key: 'address2',
-        title: 'Address 2',
-        dataType: 'text'
-      },
-      {
-        key: 'tin',
-        title: 'TIN',
-        dataType: 'text'
-      },
-      {
-        key: 'birRLFPurchaseType',
-        title: 'BIR RLF Prchs Type',
-        dataType: 'text'
-      },
-      {
-        key: 'baseAmount',
-        title: 'Base Amount',
-        dataType: 'text'
-      },
-      {
-        key: 'vatRate',
-        title: 'VAT Rate',
-        dataType: 'text'
-      },
-      {
-        key: 'inputVatAmount',
-        title: 'Input VAT Amount',
-        dataType: 'text'
-      }
-    ]
-  }
+  tempDate = '2020-12-01';
+  periodFrom = '';
+  periodTo = '';
+  extractUser = '';
+  extractDate = '';
 
-  OutputVatDetailsData: any = {
-    tableData: this.accountingService.getOutputVatDetails(),
-    tHeader: ["Tran Type","Ref. No.","Payee/Payor", "Address 1", "Address 2", "TIN","Base Amount","VAT Rate","Output VAT Amount"],
-    dataTypes: ["text","text","text","text","text","text","currency","currency","currency"],
-    resizable: [true, true, true, true, true, true,true,true,true],
-    nData: new OutputVatDetails(null,null,null,null,null,null,null,null,null),
-    pageLength: 15,
-    widths: [150,220,150,150,150,150,150,150,150],
-    total:[null,null,null,null,null,'Total','baseAmount',null,'outputVatAmount'],
-    paginateFlag:true,
-    infoFlag:true,
-    searchFlag:true,
-    filters: [
-      {
-        key: 'tranType',
-        title: 'Tran Type:',
-        dataType: 'text'
-      },
-      {
-        key: 'refNo',
-        title: 'Ref. No.',
-        dataType: 'text'
-      },
-      {
-        key: 'payeePayor',
-        title: 'Payee/Payor',
-        dataType: 'text'
-      },
-      {
-        key: 'address1',
-        title: 'Address 1',
-        dataType: 'text'
-      },
-      {
-        key: 'address2',
-        title: 'Address 2',
-        dataType: 'text'
-      },
-      {
-        key: 'tin',
-        title: 'TIN',
-        dataType: 'text'
-      },
-      {
-        key: 'baseAmount',
-        title: 'Base Amount',
-        dataType: 'text'
-      },
-      {
-        key: 'vatRate',
-        title: 'VAT Rate',
-        dataType: 'text'
-      },
-      {
-        key: 'outputVatAmount',
-        title: 'Output VAT Amount',
-        dataType: 'text'
-      }
-    ]
-  }
-
-  WithholdingVatDetailsData: any = {
-    tableData: this.accountingService.getWithholdingVATDetails(),
-    tHeader: ["Ref. No.","Payee/Payor","BIR Code","Tax Description","TIN","TIN Branch","Income","Whtax Rate","Whtax AMount"],
-    dataTypes: ["text","text","text","text","text","text","text","currency","currency"],
-    resizable: [true, true, true, true, true, true,true,true,true],
-    nData: new WithholdingVATDetails(null,null,null,null,null,null,null,null,null),
-    pageLength: 15,
-    widths: [150,220,150,150,150,150,150,150,150],
-    total:[null,null,null,null,null,'Total','income',null,'whtaxAmount'],
-    paginateFlag:true,
-    infoFlag:true,
-    searchFlag:true,
-    filters: [
-      {
-        key: 'refNo',
-        title: 'Ref. No.',
-        dataType: 'text'
-      },
-      {
-        key: 'payeePayor',
-        title: 'Payee/Payor',
-        dataType: 'text'
-      },
-      {
-        key: 'birCode',
-        title: 'BIR Code',
-        dataType: 'text'
-      },
-      {
-        key: 'taxDescription',
-        title: 'Tax Description',
-        dataType: 'text'
-      },
-      {
-        key: 'tin',
-        title: 'TIN',
-        dataType: 'text'
-      },
-      {
-        key: 'tinBranch',
-        title: 'TIN Branch',
-        dataType: 'text'
-      },
-      {
-        key: 'income',
-        title: 'Income',
-        dataType: 'text'
-      },
-      {
-        key: 'whtaxRate',
-        title: 'Whtax Rate',
-        dataType: 'text'
-      },
-      {
-        key: 'whtaxAmount',
-        title: 'Whtax Amount',
-        dataType: 'text'
-      }
-    ]
-  }
-
-
-  constructor(private accountingService: AccountingService) { }
+  constructor(private accountingService: AccountingService, private ms: MaintenanceService, private ns: NotesService) { }
 
   ngOnInit() {
+    this.getBirTaxes();
+  }
+
+  getBirTaxes() {
+    setTimeout(() => {
+      this.table.refreshTable();
+      this.table.overlayLoader = true;
+    }, 0);
+    this.ms.getExtractToCsv(this.ns.getCurrentUser(),'ACSER009',null,null,null,null,null,null,
+           null,null,null,null,null,null,null,null,null,null,
+           null,null,'S',this.taxType).subscribe(data => {
+            // data['listAcser009']
+            this.passData.tableData = data['listAcser009'];
+            if(data['listAcser009'].length > 0) {
+              this.periodFrom = data['listAcser009'][0].paramFromDate.split(' ')[0];
+              this.periodTo = data['listAcser009'][0].paramToDate.split(' ')[0];
+              this.extractUser = data['listAcser009'][0].extractUser;
+              this.extractDate = data['listAcser009'][0].extractDate.split(' ')[0];
+            }
+
+            this.table.refreshTable();
+           });
   }
 
 }
--- src/app/accounting-service/accounting-service-extract/extract-bir-tax/extract-bir-taxes/extract-bir-taxes.component.ts
+++ src/app/accounting-service/accounting-service-extract/extract-bir-tax/extract-bir-taxes/extract-bir-taxes.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { SucessDialogComponent, ModalComponent } from '@app/_components/common';
+import { NotesService, PrintService, MaintenanceService } from '@app/_services';
 
 @Component({
   selector: 'app-extract-bir-taxes',
@@ -8,24 +10,144 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ExtractBirTaxesComponent implements OnInit {
 
-
   @Output() onChange: EventEmitter<any> = new EventEmitter();
-  taxType: any = 'Creditable Withholding Tax';
+  @ViewChild('mdl') mdl: ModalComponent;
+  @ViewChild('appDialog') appDialog: SucessDialogComponent;
+  
   defaultTab: false;
 
-  constructor() { }
+  params = {
+    reportId : 'ACSER009',
+    reportName : 'ACSER009',
+    taxType: '1',
+    fromDate: '',
+    toDate: '',
+    destination: 'exl',
+    extractUser: this.ns.getCurrentUser(),
+    extractDate: ''
+  }
+
+  rType: any = 'S';
+  dialogIcon: string = "";
+  dialogMessage: string = "";
+  modalMode: string = "";
+  modalHeader: string = "";
+  modalBody: string = "";
+  loading: boolean = false;
+  passDataCsv : any[] =[];
+
+  constructor(private ns: NotesService, private ps: PrintService, private ms: MaintenanceService) { }
 
   ngOnInit() {
+    this.onChange.emit(this.params.taxType);
   }
 
-   tabController(event) {
-   	this.taxType = event.target.value;
-  	this.onChange.emit(this.taxType);
-    console.log(this.taxType);
+  taxTypeChange(event) {
+    this.onChange.emit(event.target.value);
   }
 
-  extract(){
+  onClickExtract(forceExtract?) {
+    this.params.extractDate = this.ns.toDateTimeString(0);
+    this.params['periodFrom'] = this.params.fromDate;
+    this.params['periodTo'] = this.params.toDate;
+    let param: any = JSON.parse(JSON.stringify(this.params));
+    param.forceExtract = forceExtract != undefined ? 'Y' : 'N';
     
+    this.loading = true;
+    this.ps.extractReport({ reportId: param.reportId, acser009Params: param }).subscribe((data:any)=>{
+        this.modalMode = "";
+        this.loading = false;
+        if (data.errorList.length > 0) {
+          
+          if (data.errorList[0].errorMessage.includes("parameters already exists.")) {
+            this.modalMode = "reExtract";
+            this.modalHeader = "Confirmation";
+            this.mdl.openNoClose();
+          } else {
+            this.dialogIcon = 'error';
+            this.appDialog.open();
+          }
+          
+        } else {
+          // if (data.params.extractCount != 0) {
+          if (data.returnCode == -1) {
+            this.modalHeader = "Extraction Completed";
+            // this.modalBody = "Successfully extracted " + data.params.extractCount + " record/s.";
+            this.modalBody = "Successfully extracted record/s.";
+            this.mdl.openNoClose();
+          } else {
+            this.modalHeader = "Extraction Completed";
+            this.modalBody = "No record/s extracted.";
+            this.mdl.openNoClose();
+          }
+        }
+        console.log(data)
+    },
+    (err) => {
+      alert("Exception when calling services.");
+    });
+  }
+
+  getExtractToCsv(){
+    console.log(this.params.reportId);
+      console.log(this.ns.getCurrentUser() + ' >> current user');
+      this.ms.getExtractToCsv(this.ns.getCurrentUser(),'ACSER009',null,null,null,null,null,null,
+           null,null,null,null,null,null,null,null,null,null,
+           null,null,this.rType, this.params.taxType)
+      .subscribe(data => {
+        console.log(data);
+        var months = new Array("Jan", "Feb", "Mar", 
+        "Apr", "May", "Jun", "Jul", "Aug", "Sep",     
+        "Oct", "Nov", "Dec");
+
+        alasql.fn.myFormat = function(d){
+          if(d == null){
+            return '';
+          }
+          var date = new Date(d);
+          var day = (date.getDate()<10)?"0"+date.getDate():date.getDate();
+          var mos = months[date.getMonth()];
+          return day+'-'+mos+'-'+date.getFullYear(); 
+        };
+
+        alasql.fn.negFmt = function(m){
+          return (m==null || m=='') ? 0 : Number(m);
+        };
+
+        alasql.fn.isNull = function(n){
+          return n==null?'':n;
+        };
+
+        alasql.fn.checkNullNo = function(o){
+          return (o==null || o=='')?'': Number(o);
+        };
+
+        var name = this.params.reportId;
+        var query = '';
+
+        if(this.rType == 'D'){
+          this.passDataCsv = data['listAcser009'];
+          // this.passDataCsv.forEach(a=>{a.extType = a.extType == 'N' ? 'Net' : 'Total Debit & Total Credits'});
+          // query = 'SELECT checkNullNo(extractId) as [EXTRACT ID],extractUser as [EXTRACT USER],myFormat(extractDate) as [EXTRACT DATE],checkNullNo(glAcctId) as [GL ACCT ID],'+
+          // 'isNull(acctCode) as [ACCT CODE],isNull(acctName) as [ACCT NAME],isNull(currCd) as [CURRENCY],checkNullNo(slTypeCd) as [SL TYPE CD],'+
+          // 'isNull(slTypeName) as [SL TYPE NAME],checkNullNo(slCd) as [SL CD],isNull(slName) as [SL NAME],negFmt(currency(totalCredit)) as [TOTAL CREDIT],'+
+          // 'negFmt(currency(totalDebit)) as [TOTAL DEBIT],myFormat(periodFrom) as [PERIOD FROM],myFormat(periodTo) as [PERIOD TO],isNull(currCdParam) as [CURR CD PARAM],'+
+          // 'isNull(extType) as [EXT TYPE]';
+          query = 'SELECT checkNullNo(tranId) as [TRAN ID], isNull(tranNo) as [TRAN NO], myFormat(tranDate) as [TRAN DATE], isNull(tranStat) as [TRAN STAT], isNull(status) as [STATUS], checkNullNo(payeeClassCd) as [PAYEE CLASS CD], ' +
+                  'isNull(payeeCd) as [PAYEE CD], isNull(payeeName) as [COMPANY NAME], checkNullNo(bussTypeCd) as [BUSS TYPE CD], isNull(bussTypeName) as [BUSS TYPE NAME], isNull(tin) as [TIN], isNull(mailAddress) as [ADDRESS], ' +
+                  'negFmt(currency(baseAmt)) as [BASE AMT], negFmt(currency(taxAmt)) as [TAX AMT], isNull(taxCd) as [TAX CODE], negFmt(currency(rate)) as [RATE]';
+
+          this.ns.export('ACSER009B', query, this.passDataCsv);
+        } else if(this.rType == 'S'){
+          this.passDataCsv = data['listAcser009'];
+          query = 'SELECT isNull(tranNo) as [TRAN NO], myFormat(tranDate) as [TRAN DATE], isNull(payeeName) as [COMPANY NAME], isNull(tin) as [TIN], isNull(mailAddress) as [ADDRESS], ' +
+                  'negFmt(currency(baseAmt)) as [BASE AMT], negFmt(currency(taxAmt)) as [TAX AMT], isNull(taxCd) as [TAX CODE], negFmt(currency(rate)) as [RATE]';
+
+          this.ns.export('ACSER009A', query, this.passDataCsv);
+        }
+
+        console.log(this.passDataCsv);
+      });
   }
 
 }
